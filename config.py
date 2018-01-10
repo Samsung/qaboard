1581002fb6b65ea49bbf07fda6a47441d624d5df
Arthur Flam <arthur.flam@samsung.com>
2018-01-10 14:06:18 +0200

    updated the default config

--- config.py
+++ config.py
@@ -7,9 +7,9 @@ app_data_directory = Path(os.getenv('SLAMVVIZAPP_APP_DATA_PATH', '.'))
 
 # unix config
 ci_directory = Path('/home/arthurf/ci/dvs/psp_swip')
-default_recordings_directory  = Path('/stage/algo_data/Sebastien/DVS_SLAM/Database/')
+default_recordings_directory  = Path('/stage/algo_archive/DVS_SLAM_Database/')
 
 # windows config
 if os.name == 'nt':
 	ci_directory = Path('//mars/homes/arthurf/ci/dvs/psp_swip')
-	default_recordings_directory  = Path('//netapp/algo_data/Sebastien/DVS_SLAM/Database/')
+	default_recordings_directory  = Path('//f2/algo_archive/DVS_SLAM_Database/')
