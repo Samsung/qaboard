fcae0c0d73546681f920d16c8036e4831e58e0a4
Arthur Flam <arthur.flam@samsung.com>
2018-01-22 15:53:35 +0200

    added a tentative JS app for tuning based on create-react-app

---
+++ slamvizapp-tuning/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+render(<App/>, document.getElementById('root'));
+registerServiceWorker();
