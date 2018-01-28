6a1d70fe07e487fb567bb8b57be1de9649852e1d
Arthur Flam <arthur.flam@samsung.com>
2018-01-28 15:05:40 +0200

    improved the speed of the cleaning script by a lot. Now removing old movies also.,

--- slamvizapp/scripts/clean.py
+++ slamvizapp/scripts/clean.py
@@ -5,53 +5,64 @@ Removes the outputs from old commits from the disk. This saves storage...
 import datetime
 import subprocess
 import click
+from gitdb.exc import BadName
 
 from slamvizapp import repo, db_session
 from slamvizapp.models import CiCommit
 from slamvizapp.config import *
 
 
+
+
 @click.command()
-@click.option('--days', default=7, help='Outputs folders older than this will be removed.')
-@click.option('--branch', default=None, help='Delete only from this branch.')
-def clean(days, branch):
+@click.option('--days', default=5, help='Outputs folders older than this will be removed.')
+def clean(days):
   """Removes the outputs from old commits from the disk. This saves storage..."""
+  now = datetime.datetime.now().astimezone()
+  threshold = datetime.timedelta(days=days)
+  def is_old(commit):
+    return now - commit.authored_datetime > threshold
+
+
   # We don't want to touch the commits from that branch
   protected_refs = set(['origin/develop'])
   protected_commits = set()
   for ref in protected_refs:
     for c in repo.iter_commits(ref):
       protected_commits.add(c)
-
   # for c in protected_commits:
   #   print(f'{c.hexsha} on {c.authored_datetime} by {c.author.name}')
   print(f'{len(protected_commits)} protected')
 
-  now = datetime.datetime.now().astimezone()
-  threshold = datetime.timedelta(days=days)
 
-  references = [branch] if branch else repo.refs
-  old_commits = set()
-  for ref in references:
-    for c in repo.iter_commits(ref):
-      is_old = now - c.authored_datetime > threshold
-      if is_old and c not in protected_commits:
-        old_commits.add(c)
-  print(f'{len(old_commits)} to be deleted')
+  cicommits_dir = ci_directory/'commits'
+  for cicommit_dir in cicommits_dir.glob('*__git__*'):
+    commit_short_id = str(cicommit_dir)[-8:]
+    try:
+      commit = repo.commit(commit_short_id)
+    except BadName:
+      print(cicommit_dir)
+      continue
 
-  for c in old_commits:
-    print(f'DELETE: {c.hexsha} on {c.authored_datetime} by {c.author.name}')
-    commit_dir = ci_directory / 'commits' / f'{c.authored_date}__git__{c.hexsha[:8]}'
-    if commit_dir.exists():
-      command = f'rm -rf {str(commit_dir)}'
-      subprocess.Popen([command], shell=True)
-      # outputs = ci_commit.commit_dir/'output'
-      # logs = ci_commit.commit_dir/'lsf.log'
-      # command = f'rm -rf {str(outputs)} {str(logs)}'
-      # print(command)
+    if is_old(commit):
+      if commit not in protected_commits:
+        print(f'DELETE: {commit.hexsha} on {commit.authored_datetime} by {commit.author.name}')
+        subprocess.Popen(f'rm -rf {cicommit_dir}', shell=True)
+      else:
+        subprocess.Popen(f"find {cicommit_dir} -name '*mp4' -delete -print", shell=True)
 
-
+  # we remove core dumps, they are soooo heavy...
+  # we could update the LSF params to avoid creating them at all I guess
   subprocess.Popen("find /home/arthurf/ci/dvs/psp_swip/branches -maxdepth 3 -name '*core*' -delete", shell=True)
+
+
+def remove_outputs(commit):
+  pass
+
+
+def remove_videos(commit):
+  pass
+
 
 
 
