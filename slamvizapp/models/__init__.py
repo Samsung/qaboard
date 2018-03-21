7cce3f8e998a7e3bef513a7ebcc27643f7b9c48d
Arthur Flam <arthur.flam@samsung.com>
2018-03-21 09:33:25 +0200

    renamed file

--- slamvizapp/models/__init__.py
+++ slamvizapp/models/__init__.py
@@ -1,7 +1,7 @@
 from slamvizapp.database import Base, engine
 
 from .Recording import *
-from .ParametersSet import *
+from .Parameters import *
 from .SlamOutput import *
 from .Batch import *
 from .CiCommit import *
