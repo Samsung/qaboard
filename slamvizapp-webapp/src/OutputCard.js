a6fa4debbb782035981db0c0e0f97e3ab5f88c71
Arthur Flam <arthur.flam@samsung.com>
2018-03-26 14:50:30 +0300

    more understandable icon

--- slamvizapp-webapp/src/OutputCard.js
+++ slamvizapp-webapp/src/OutputCard.js
@@ -171,7 +171,7 @@ class OutputCard extends Component {
       <Tag intent={Intent.PRIMARY} className="pt-round pt-minimal">{output_new.platform}</Tag>
       <Tag intent={Intent.PRIMARY} className="pt-round pt-minimal">{output_new.configuration}</Tag>
       <Button onClick={this.toogleShowDebug} className="pt-minimal" style={{paddingLeft: '12px'}} text="toogle debug plots" intent={showDebug ? Intent.PRIMARY : Intent.NONE} iconName="series-add" />
-      <a title="Show output files" style={{paddingLeft: '8px'}} target="_blank" href={output_new.output_dir_url}><Icon iconName="folder-shared"/></a>
+      <a title="Show output files" style={{paddingLeft: '8px'}} target="_blank" href={output_new.output_dir_url}><Icon iconName="download"/></a>
     </span>
 
     return <Fragment> {!output_new.is_failed && !output_new.is_pending &&
