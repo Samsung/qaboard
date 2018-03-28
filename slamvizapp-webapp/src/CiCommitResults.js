e8ebad6ae1fa80595ec7da2b17d7c70516e2165e
Arthur Flam <arthur.flam@samsung.com>
2018-03-28 13:53:56 +0300

    removed extra comma

--- slamvizapp-webapp/src/CiCommitResults.js
+++ slamvizapp-webapp/src/CiCommitResults.js
@@ -161,7 +161,7 @@ class Tuning extends Component {
       platform: 'lsf',
 
       selected_group: null,
-      tuning_search: `{\n  "search_type": "grid",\n  "parameter_search": {\n    "frame_duration": [10.0, 20.0, 30.0],\n  }\n}\n`,
+      tuning_search: `{\n  "search_type": "grid",\n  "parameter_search": {\n    "frame_duration": [10.0, 20.0, 30.0]\n  }\n}\n`,
     };
   }
 
