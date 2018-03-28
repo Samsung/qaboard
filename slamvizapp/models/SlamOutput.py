a766b0a2f2c7a3431d1556f2466b9f16ae25e1f4
Arthur Flam <arthur.flam@samsung.com>
2018-03-28 09:42:57 +0300

    new directory layout for tuning

--- slamvizapp/models/SlamOutput.py
+++ slamvizapp/models/SlamOutput.py
@@ -118,14 +118,11 @@ class SlamOutput(Base):
   @property
   def output_folder(self):
     if self.batch.label != 'default':
-      if len(self.extra_parameters) == 0:
-        parameters_folder = Path('default') / 'default'
-      else:
-        parameters_s = json.dumps(self.extra_parameters, sort_keys=True)
-        parameters_folder = hashlib.md5(parameters_s.encode()).hexdigest()
+      parameters_s = json.dumps(self.extra_parameters, sort_keys=True)
+      parameters_hash = hashlib.md5(parameters_s.encode()).hexdigest()
     else:
       parameters_folder = ''
-    return Path(self.platform) / self.configuration / parameters_folder[:2] / parameters_folder / self.recording.output_folder
+    return Path(self.platform) / self.configuration / parameters_hash[:2] / parameters_hash / self.recording.output_folder
 
   @property
   def output_dir(self):
