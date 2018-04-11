675854e02a26cfd9240261099658bae0e69bf675
Arthur Flam <arthur.flam@samsung.com>
2018-04-11 17:28:09 +0300

    localizing defaults

--- slamvizapp/api.py
+++ slamvizapp/api.py
@@ -99,8 +99,9 @@ def get_commits(branch=None):
   to_datetime = lambda s: timezone.localize(datetime.datetime.strptime(s, '%Y-%d-%mT%H:%M:%S.%fZ'))
   from_date_s = request.args.get('from', None)
   to_date_s = request.args.get('to', None)
-  from_date = to_datetime(from_date_s) if from_date_s else (datetime.datetime.now() - datetime.timedelta(days=3))
-  to_date = to_datetime(to_date_s) if to_date_s else datetime.datetime.now()
+  now_localized = timezone.localize(datetime.datetime.now())
+  from_date = to_datetime(from_date_s) if from_date_s else (now_localized - datetime.timedelta(days=3))
+  to_date = to_datetime(to_date_s) if to_date_s else now_localized
   committer_name = request.args.get('committer', None)
 
   if not branch:
