66758a6f00c06aec76a56ae94b9e170432b71d25
Arthur Flam <arthur.flam@samsung.com>
2018-04-11 16:37:43 +0300

    fixed syntax

--- slamvizapp/api.py
+++ slamvizapp/api.py
@@ -109,7 +109,7 @@ def get_commits(branch=None):
                      .filter(CiCommit.authored_datetime <= to_date,
                              CiCommit.authored_datetime >= from_date
                             )\
-                     .order_by(CiCommit.authored_datetime.desc())\
+                     .order_by(CiCommit.authored_datetime.desc())
     else:
       ci_commits = CiCommit.query\
         .filter_by(committer_name=committer_name)\
