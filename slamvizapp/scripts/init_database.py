82cce963042c40c708e9653f931e7537bcb472fa
Arthur Flam <arthur.flam@samsung.com>
2018-04-15 15:06:32 +0300

    added schema stamp in the init_db script

--- slamvizapp/scripts/init_database.py
+++ slamvizapp/scripts/init_database.py
@@ -4,16 +4,21 @@ Initializes or updates the database using information from the filesystem.
 """
 import time
 import datetime
+from pathlib import Path
 
 import click
 from git.exc import BadName
 from sqlalchemy.orm.exc import NoResultFound
+from alembic.config import Config
+from alembic import command
 
 from slamvizapp import repo
 from slamvizapp.database import engine, Session
 from slamvizapp.models import Base, CiCommit, Recording, Batch, SlamOutput
 from slamvizapp.config import default_recordings_directory, ci_directory
 
+import os
+import slamvizapp
 
 def print_summary():
   session = Session()
@@ -22,6 +27,15 @@ def print_summary():
   print(f'total Batches: {session.query(Batch).count()}')
   print(f'total SlamOutputs: {session.query(SlamOutput).count()}')
 
+
+def stamp_schema_version():
+  with engine.begin() as connection:
+    alembic_cfg = Config()
+    alembic_cfg.set_main_option("script_location", "slamvizapp:alembic")
+    path = Path(slamvizapp.__file__).parent / 'alembic.ini'
+    alembic_cfg.config_file_name = str(path)
+    alembic_cfg.attributes['connection'] = connection
+    command.stamp(alembic_cfg, "head")
 
 @click.command()
 @click.option('--drop-all', is_flag=True)
@@ -33,9 +47,12 @@ def init_database(drop_all, loop, sleep, verbose):
     # for tbl in reversed(Base.metadata.sorted_tables):
     # engine.execute(tbl.delete())
     if verbose: print('dropping all data')
-    # Base.metadata.drop_all(engine)
+    Base.metadata.drop_all(engine)
   if verbose: print('creating schema...')
   Base.metadata.create_all(engine)
+
+  stamp_schema_version()
+
   # Recordings will be created already when importing CiCommits
   # init_recordings()
   init_cicommits(verbose=verbose)
