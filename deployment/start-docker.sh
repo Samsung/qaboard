1788d32ce7a4b6bcebc9907c88b103fe2320a397
Arthur Flam <arthur.flam@samsung.com>
2018-05-15 17:28:43 +0300

    SSL..

--- deployment/start-docker.sh
+++ deployment/start-docker.sh
@@ -73,7 +73,7 @@ else
 fi
 
 # ! we already copy the whole nginx config folder in the dockerfile... that's not great.
-DOCKER_VOLUMES+=" --volume=$HOME_DOCKER/dvs/slamvizapp/deployment/nginx/ssl/dvs:/slamvizapp/deployment/nginx/ssl/dvs"
+# DOCKER_VOLUMES+=" --volume=$HOME_DOCKER/dvs/slamvizapp/deployment/nginx/ssl/dvs:/slamvizapp/deployment/nginx/ssl/dvs"
 # DOCKER_VOLUMES+=" --volume=$HOME_DOCKER/dvs/slamvizapp/deployment/init.sh:/slamvizapp/deployment/init.sh"
 
 # Networking:
