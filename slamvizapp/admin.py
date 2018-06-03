38db35e553c901b7052a6165b760838228f5716e
Arthur Flam <arthur.flam@samsung.com>
2018-06-03 13:30:51 +0300

    The admin interface is back

--- slamvizapp/admin.py
+++ slamvizapp/admin.py
@@ -5,6 +5,7 @@ https://flask-admin.readthedocs.io/en/latest/introduction/#getting-started
 """
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
+from flask_admin.form import fields as fa_fields
 
 from slamvizapp import app, db_session
 from slamvizapp.models import TestInput, CiCommit, Batch, Project 
@@ -19,29 +20,28 @@ admin = Admin(app, name='slamvizapp',
 
 # we don't want to display those
 one_to_many_columns = ['outputs',]
-editable_columns = set(c.name for c in TestInput.metadata.tables['test_inputs'].columns
-                      ) - set(['path', 'database'])
-
+columns = set(c.name for c in TestInput.metadata.tables['test_inputs'].columns)
+editable_columns = columns - set(['path', 'database', 'data'])
 class TestInputModelView(ModelView):
   page_size = 50
-
-  # can_create = False
-  # can_edit = False
-  # can_delete = False
   create_modal = True
   edit_modal = True
-
   column_exclude_list = one_to_many_columns
   form_excluded_columns = one_to_many_columns
-
   can_view_details = True
-  column_searchable_list = ['path']
+  column_searchable_list = ['path', 'database']
   column_filters = editable_columns
   column_editable_list = editable_columns
+  form_override = dict(
+      json=fa_fields.JSONField
+  )
 
+class ProjectModelView(ModelView):
+  column_list = ['id', 'information']
+  can_delete = False
 
 
 admin.add_view(TestInputModelView(TestInput, db_session))
 admin.add_view(ModelView(CiCommit, db_session))
 admin.add_view(ModelView(Batch, db_session))
-admin.add_view(ModelView(Project, db_session))
+admin.add_view(ProjectModelView(Project, db_session))
