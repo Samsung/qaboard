b612c26d7fa0d430275ac29343bc4a786d3a22c9
Arthur Flam <arthur.flam@samsung.com>
2018-06-19 13:12:45 +0300

    Added the option to import in a specific batch with $datetime__local__avi__$commit_EXPORT:my-label_$message

--- slamvizapp/scripts/slam/load_manual_android_runs.py
+++ slamvizapp/scripts/slam/load_manual_android_runs.py
@@ -38,7 +38,8 @@ re_datetime = '(?P<time>[0-9]{4}-[0-9]{2}-[0-9]{2}_[0-9]{2}-[0-9]{2}-[0-9]{2})'
 re_author = '(?P<author>[A-Za-z0-9]*)'
 re_commit_id = '(?P<commit_id>[A-Za-z0-9]*)'
 re_message = '(?P<message>.*)'
-id_parser = re.compile(f'^{re_datetime}__local__{re_author}__EXPORT_{re_commit_id}(?:_{re_message})*')
+re_label = '((?P<label>[a-zA-Z0-9-]))?'
+id_parser = re.compile(f'^{re_datetime}__local__{re_author}__EXPORT{re_label}_{re_commit_id}(?:_{re_message})*')
 
 def init_slam_manual_runs(verbose=False):
   for folder in root.iterdir():
@@ -47,6 +48,7 @@ def init_slam_manual_runs(verbose=False):
     if not matches: continue
     matches = matches.groupdict()
     commid_id = matches['commit_id']
+    label = matches['label'] if matches['label'] else 'ci-android-rt'
     if commid_id=='PC': continue
     if commid_id=='Android': continue
 
@@ -59,7 +61,7 @@ def init_slam_manual_runs(verbose=False):
 
 
 
-def import_slam_manual_run(folder, commit_short_id, verbose=False):
+def import_slam_manual_run(folder, commit_short_id, batch_label, verbose=False):
   session = Session()
   project = Project.get_or_create(session=session, id='dvs/psp_swip')
   repo = repos[project.id]
@@ -73,7 +75,7 @@ def import_slam_manual_run(folder, commit_short_id, verbose=False):
   commit = repo.commit(commit_short_id)
   ci_commit = session.query(CiCommit).filter_by(id=commit.hexsha).one()
   # if verbose: print(f'  {ci_commit}')
-  batch_android = ci_commit.get_or_create_batch('ci-android-rt')
+  batch_android = ci_commit.get_or_create_batch(batch_label)
 
   output_dirs = [p.parent for p in folder_path.rglob('metrics.json')]
   # print(folder_path)
