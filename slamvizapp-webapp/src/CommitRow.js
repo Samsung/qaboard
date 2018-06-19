e4be8fed9050200aee139a6d308ec4834fbacdc5
Arthur Flam <arthur.flam@samsung.com>
2018-06-19 13:13:32 +0300

    Fixed a typo

--- slamvizapp-webapp/src/CommitRow.js
+++ slamvizapp-webapp/src/CommitRow.js
@@ -121,7 +121,7 @@ const CommitShortId = styled.a`
 class CommitRow extends React.Component {
   render() {
     const {commit, project, className, toaster} = this.props;
-    const commit_url = (project === 'dvs/psp_swip' || project === 'dvs/swip_tof') ? `http://gitlab-srv/${project}/commit/${commit.id}` : '#sorry-not-yet-available';
+    const commit_url = (project === 'dvs/psp_swip' || project === 'tof/swip_tof') ? `http://gitlab-srv/${project}/commit/${commit.id}` : '#sorry-not-yet-available';
     return (
       <CommitRowWrapper className={className}>
         <Avatar alt={commit.committer_name} href={`/committer/${commit.committer_name}?project=${project}`} src={commit.committer_avatar_url} />
