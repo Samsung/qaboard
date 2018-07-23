e5f82d1fbe380fdff8455ed05c39575dc142663d
Arthur Flam <arthur.flam@samsung.com>
2018-07-23 17:33:35 +0300

    Using the reference branch from qatools

--- slamvizapp/api/api.py
+++ slamvizapp/api/api.py
@@ -137,10 +137,15 @@ def get_ci_commit(commit_id=None):
     commit_id = request.args.get('commit', None)
 
   if not commit_id:
-    branch = request.args.get('branch', 'origin/develop')
+    try:
+      project = Project.query.filter(Project.id==project_id).one()
+      default_branch = project.information['qatools_config']['project']['reference_branch']
+    except:
+      default_branch = 'origin/develop'
+    branch = request.args.get('branch', default_branch)
     ci_commit = latest_successful_commit(db_session, project_id=project_id, branch=branch)
     if not ci_commit:
-      return jsonify({'error': 'Sorry, we cant find any commit with results for this project.'}), 404
+      return jsonify({'error': f'Sorry, we cant find any commit with results for this project on {branch}.'}), 404
   else:
     try: # we try a commit from git
       if project_id == 'dvs/psp_swip':
