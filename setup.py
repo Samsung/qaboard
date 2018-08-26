f9d3608636dfc7d938cb349c0b517de7302b9417
Arthur Flam <arthur.flam@samsung.com>
2018-08-26 17:01:31 +0300

    changed the entrypoint

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
 
   entry_points='''
       [console_scripts]
-      qa=qatools.qatools:cli
+      qa=qatools.qatools:main
   ''',
 
   # https://setuptools.readthedocs.io/en/latest/setuptools.html#including-data-files
