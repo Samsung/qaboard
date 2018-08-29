7dddf38500205a8e5c2d5fcb3a9331f4e119429c
Arthur Flam <arthur.flam@samsung.com>
2018-08-29 12:42:17 +0300

    fix

--- slamvizapp/api/webhooks.py
+++ slamvizapp/api/webhooks.py
@@ -76,7 +76,7 @@ def new_output_webhook():
     metrics = request.json.get('metrics', {})
     if not metrics: # we look for metrics.json in the output directory
       output.update_metrics()
-    else 
+    else:
       output.metrics = metrics
 
   db_session.add(output)
