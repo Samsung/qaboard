5c9dd37215580145d883a8442d867f9db8f90a04
Arthur Flam <arthur.flam@samsung.com>
2018-09-25 09:54:32 +0300

    Refactored for clarity, and avoid needed a manual setup for new projects

--- slamvizapp/api/tuning.py
+++ slamvizapp/api/tuning.py
@@ -15,144 +15,212 @@ from ..utils import iter_recordings
 from ..config import shared_data_directory
 
 
+def get_groups_path(project_id):
+    """
+    Return the path of the file where we save the groups of tests we defined for a project.
+    Creates it if it does not exist yet.
+    """
+    path = shared_data_directory / project_id / "extra-batches.yml"
+    if not path.exists():
+        path.parent.mkdir(parents=True, exist_ok=True)
+        with path.open("w") as f:
+            f.write(
+                """
+                # you can define groups of tests using this syntax:
+                # http://gitlab-srv/common-infrastructure/qatools/blob/master/qatools/sample_project/qatools/input_groups.yaml"""
+            )
+    return path
 
 
-@app.route("/api/v1/recordings/groups", methods=['GET', 'POST'])
+@app.route("/api/v1/recordings/groups", methods=["GET", "POST"])
 def groups():
-  project_id = request.args.get('project', 'dvs/psp_swip')
-  recording_groups_filepath = shared_data_directory / project_id / 'extra-batches.yml'
-  if request.method == 'POST':
-    data = request.get_json()
-    recording_groups_filepath.parent.mkdir(parents=True, exist_ok=True)
-    with recording_groups_filepath.open('w') as f:
-      f.write(data['groups'])
-    return jsonify('OK')
-  else:
-    try:
-      with (recording_groups_filepath).open('r') as f:
-        return f.read()
-    except:
-      return  jsonify({"error": f"Could not open or read {recording_groups_filepath}"}), 500
+    """
+    Return or update the groups of tests we defined for a project.
+    TODO: We could just make it part of the database, why bother with files...
+          It could be saved as test as project.informations.test_groups
+          We would *just* need to write the migration, and it would save 30 lines of code.
+    """
+    project_id = request.args.get("project", "dvs/psp_swip")
+    groups_path = get_test_groups_path(project_id)
+    if request.method == "POST":
+        data = request.get_json()
+        with groups_path.open("w") as f:
+            f.write(data["groups"])
+        return jsonify("OK")
+    else:
+        try:
+            with groups_path.open("r") as f:
+                return f.read()
+        except:
+            return (
+                jsonify(
+                    {"error": f"Could not open or read {groups_path}"}
+                ),
+                500,
+            )
 
 
 @app.route("/api/v1/recordings/group")
 def get_group():
-  project_id = request.args.get('project', 'dvs/psp_swip')
-  project = Project.get_or_create(session=db_session, id=project_id)
-  recording_groups_filepath = shared_data_directory / project_id / 'extra-batches.yml'
-  try:
-    is_legacy_project = project_id in ['dvs/psp_swip', 'tof/swip_tof']
-    if is_legacy_project:
-      recordings = list(iter_recordings(
-        [request.args.get('name', '')],
-        recording_groups_filepath,
-        project.database
-      ))
-    else:
-      import qatools.utils
-      test = [request.args.get('name', '')]
-      recordings = list(qatools.utils.iter_recordings(
-        [request.args.get('name', '')],
-        recording_groups_filepath,
-        project.database,
-        project.information['qatools_config']['inputs']['configuration'],  
-        project.information['qatools_config'],
-      ))
-    return jsonify({'number_of_recordings': len(recordings)})
-  except:
-    return jsonify({'number_of_recordings': 0})    
-
-
-@app.route("/api/v1/commit/<hexsha>/batch", methods=['POST'])
-@app.route("/api/v1/commit/<hexsha>/batch", methods=['POST'])
-def add_batch(hexsha):
-  project_id = request.args.get('project', 'dvs/psp_swip')
-  try:
-    commit = repos[project_id].commit(hexsha)
-    ci_commit = (CiCommit
-                 .query.filter(
-                   CiCommit.project_id==project_id,
-                   CiCommit.id.startswith(hexsha),
-                 )
-                 .one()
+    project_id = request.args.get("project", "dvs/psp_swip")
+    project = Project.get_or_create(session=db_session, id=project_id)
+    groups_path = get_groups_path(project_id)
+    try:
+        # FIXME: use qatools for those projects, and remove this code
+        # that does the same (but with a different signature, and doesn't parse per-test config)
+        is_legacy_project = project_id in ["dvs/psp_swip", "tof/swip_tof"]
+        if is_legacy_project:
+            recordings = list(
+                iter_recordings(
+                    [request.args.get("name", "")],
+                    groups_path,
+                    project.database,
                 )
-  except NoResultFound:
-    return jsonify("Sorry, the commit id was not found"), 404
+            )
+        else:
+            import qatools.utils
 
-  is_legacy_project = project_id in ['dvs/psp_swip', 'tof/swip_tof']
-  if 'qatools_config' not in ci_commit.project.information and not is_legacy_project:
-    return jsonify("Please configure `qatools first`"), 404
+            test = [request.args.get("name", "")]
+            recordings = list(
+                qatools.utils.iter_recordings(
+                    [request.args.get("name", "")],
+                    groups_path,
+                    project.database,
+                    project.information["qatools_config"]["inputs"]["configuration"],
+                    project.information["qatools_config"],
+                )
+            )
+        return jsonify({"number_of_recordings": len(recordings)})
+    except:
+        return jsonify({"number_of_recordings": 0})
 
 
-  data = request.get_json()
-  recording_groups_filepath = shared_data_directory / project_id / 'extra-batches.yml'
+@app.route("/api/v1/commit/<hexsha>/batch", methods=["POST"])
+@app.route("/api/v1/commit/<hexsha>/batch", methods=["POST"])
+def add_batch(hexsha):
+    """
+    Request that we run extra tests for a given project.
+    """
+    project_id = request.args.get("project", "dvs/psp_swip")
+    project_dir = shared_data_directory / project_id
+    if not project_dir.exists(): project_dir.mkdir(exist_ok=True, parents=True)
 
-  ci_commit.time_of_last_batch = datetime.datetime.now().astimezone()
-  db_session.add(ci_commit)
-  db_session.commit()
+    try:
+        commit = repos[project_id].commit(hexsha)
+        ci_commit = CiCommit.query.filter(
+            CiCommit.project_id == project_id, CiCommit.id.startswith(hexsha)
+        ).one()
+    except NoResultFound:
+        return jsonify("Sorry, the commit id was not found"), 404
 
-  overwrite = '--overwrite' if data['overwrite'] == 'on' else ''
-  # to avoid issues with quoting, we create a temporary file to describe the job
-  if is_legacy_project:
-    batch_command = ' '.join([
-      'python tools/performance-evaluation/run.py',
-      f"--platform '{data['platform']}'" if 'platform' in data else '',
-      f"--configuration '{data['configuration']}'" if 'configuration' in data else '',
-      f"--batch-label '{data['batch_label']}'",
-      'batch',
-      f'--recording-groups-file {recording_groups_filepath}',
-      f"--recording-group '{data['selected_group']}'",
-      f"--tuning-search '{json.dumps(data['tuning_search'])}'",
-      f'{overwrite}',
-      f'--no-wait',
-      '\n',
-    ])
-    working_directory = ci_commit.project.ci_directory / project_id / 'branches' / 'develop' / project_id.split('/')[1]
-  else:
-    batch_command = ' '.join([
-      'qa',
-      f"--platform '{data['platform']}'" if 'platform' in data else '',
-      f"--configuration '{data['configuration']}'" if 'configuration' in data else '',
-      f"--batch-label '{data['batch_label']}'",
-      'batch',
-      f'--groups-file {recording_groups_filepath}',
-      f"--group '{data['selected_group']}'",
-      f"--tuning-search '{json.dumps(data['tuning_search'])}'",
-      f'{overwrite}',
-      f'--no-wait',
-      '\n',
-    ])
-    config = ci_commit.project.information['qatools_config']
-    working_directory = ci_commit.commit_dir
-  print(working_directory)
-  print(batch_command)
+    is_legacy_project = project_id in ["dvs/psp_swip", "tof/swip_tof"]
+    if "qatools_config" not in ci_commit.project.information and not is_legacy_project:
+        return jsonify("Please configure `qatools first`"), 404
 
-  queue = 'alg_q' if is_legacy_project else ci_commit.project.information['qatools_config']['lsf']['fast_queue']
-  # openstf is our device farm
-  use_openstf = data['android_device'].lower() == 'openstf'
-  user = data['user'] if 'user' in data else 'arthurf'
-  batch_script = ''.join([
-    '#!/bin/bash\n',
-    f'bsub_su {user} -q {queue} -sp 4000 ', # highest priority
-    f'-o /home/arthurf/dvs/slamvizapp/data/{project_id}/lsf.log ',
-    '<< EOF\n'
-    f'  cd "{working_directory}";\n',
-    # android options
-    f"  export RESERVED_ANDROID_DEVICE='{data['android_device']}';\n" if not use_openstf else '',
-    f"  export OPENSTF_STORAGE_QUOTA=12;\n" if not use_openstf else '',
-    f"  export {'SAMSUNG_CI_COMMIT_DIR' if is_legacy_project else 'QATOOLS_CI_COMMIT_DIR'}='{ci_commit.commit_dir}';\n",
-    f"  export CI_COMMIT_SHA='{ci_commit.gitcommit.hexsha}';\n  ",
-    batch_command,
-    'EOF',
-  ])
-  print(batch_script)
-  now = datetime.datetime.now().timestamp()
-  batch_script_directory = Path(f'/home/arthurf/dvs/slamvizapp/data/batches/{project_id}')
-  batch_script_directory.mkdir(exist_ok=True, parents=True)
-  batch_script_filepath = batch_script_directory/f'{ci_commit.gitcommit.hexsha}_{now}.sh'
-  with batch_script_filepath.open('w') as f:
-    f.write(batch_script)
-  cmd = f'ssh -tt -o StrictHostKeyChecking=no -i /home/arthurf/.ssh/ispq.id_rsa ispq@planet31 bash {batch_script_filepath}'
-  print(cmd)
-  subprocess.run(cmd, shell=True, encoding='utf-8')
-  return jsonify({'command': batch_script})
+    groups_path = get_groups_path(project_dir)
+
+    ci_commit.time_of_last_batch = datetime.datetime.now().astimezone()
+    db_session.add(ci_commit)
+    db_session.commit()
+
+    data = request.get_json()
+
+    overwrite = "--overwrite" if data["overwrite"] == "on" else ""
+    if is_legacy_project:
+        batch_command = " ".join(
+            [
+                "python tools/performance-evaluation/run.py",
+                f"--platform '{data['platform']}'" if "platform" in data else "",
+                f"--configuration '{data['configuration']}'"
+                if "configuration" in data
+                else "",
+                f"--batch-label '{data['batch_label']}'",
+                "batch",
+                f"--recording-groups-file {groups_path}",
+                f"--recording-group '{data['selected_group']}'",
+                f"--tuning-search '{json.dumps(data['tuning_search'])}'",
+                f"{overwrite}",
+                f"--no-wait",
+                "\n",
+            ]
+        )
+        working_directory = (
+            ci_commit.project.ci_directory
+            / project_id
+            / "branches"
+            / "develop"
+            / project_id.split("/")[1]
+        )
+    else:
+        batch_command = " ".join(
+            [
+                "qa",
+                f"--platform '{data['platform']}'" if "platform" in data else "",
+                f"--configuration '{data['configuration']}'"
+                if "configuration" in data
+                else "",
+                f"--batch-label '{data['batch_label']}'",
+                "batch",
+                f"--groups-file {groups_path}",
+                f"--group '{data['selected_group']}'",
+                f"--tuning-search '{json.dumps(data['tuning_search'])}'",
+                f"{overwrite}",
+                f"--no-wait",
+                "\n",
+            ]
+        )
+        config = ci_commit.project.information["qatools_config"]
+        working_directory = ci_commit.commit_dir
+    print(working_directory)
+    print(batch_command)
+
+
+    # To avoid issues with quoting, we create a temporary file to describe the job
+    # We could also play with heredocs-within-heredocs, but it is painful,
+    # and this way we get logs
+    user = data["user"] if "user" in data else "arthurf"
+    queue = "alg_q" if is_legacy_project else ci_commit.project.information["qatools_config"]["lsf"]["fast_queue"]
+    # openstf is our Android device farm
+    use_openstf = data["android_device"].lower() == "openstf"
+    batch_script = "".join(
+        [
+            "#!/bin/bash\n",
+            f"bsub_su {user} -q {queue} -sp 4000 ",  # highest priority
+            f"-o {project_dir}/lsf.log ",
+            "<< EOF\n" f'  cd "{working_directory}";\n',
+            # options specific to android
+            f"  export RESERVED_ANDROID_DEVICE='{data['android_device']}';\n" if not use_openstf else "",
+            f"  export OPENSTF_STORAGE_QUOTA=12;\n" if not use_openstf else "",
+            f"  export {'SAMSUNG_CI_COMMIT_DIR' if is_legacy_project else 'QATOOLS_CI_COMMIT_DIR'}='{ci_commit.commit_dir}';\n",
+            f"  export CI_COMMIT_SHA='{ci_commit.gitcommit.hexsha}';\n  ",
+            batch_command,
+            "EOF",
+        ]
+    )
+    print(batch_script)
+
+    script_path = project_dir / f"{ci_commit.gitcommit.hexsha}_{datetime.datetime.now()}.sh"
+    with script_path.open("w") as f:
+        f.write(batch_script)
+    cmd = " ".join(
+        [
+            "ssh",
+            # quiet to avoid the welcome banner
+            "-q",
+            # ask, and force a TTY, otherwise bsub->su will complain
+            "-tt",
+            # make sure we OK the server key during the first-connection
+            "-o StrictHostKeyChecking=no",
+            # ispq is the only user that can use bsub_su, an alias for su {0} {1:}.
+            "-i /home/arthurf/.ssh/ispq.id_rsa",
+            "ispq@planet31",
+            f"bash {script_path}",
+        ]
+    )
+    print(cmd)
+    try:
+        out = subprocess.run(cmd, shell=True, encoding="utf-8", stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out.check_returncode()
+    except subprocess.CalledProcessError:
+        return jsonify({"error": "{out.stdout}\n{out.stderr}"}), 500
+    return jsonify({"script": str(script_path)})
