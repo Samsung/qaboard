6a859e694087f4aebe25f99988559fe012f2dd6b
Arthur Flam <arthur.flam@samsung.com>
2018-10-02 13:40:01 +0300

    More verbose

--- qatools/tuning.py
+++ qatools/tuning.py
@@ -195,13 +195,15 @@ def batch_objective(batch_label, config_objective):
       else:
         metric_target = None
       print(output)
-      if output.is_failed:
-        raise
-      try:
-        losses.append(loss(output['metrics'][metric], metric_target) )
-      except:
-        click.secho('Could not find {metric}', fg='red')        
+      if output['is_failed']:
+        click.secho('Failed output', fg='red')        
         click.secho(str(output), fg='red')
+      else:
+        try:
+          losses.append(loss(output['metrics'][metric], metric_target) )
+        except:
+          click.secho('Could not find {metric}', fg='red')        
+          click.secho(str(output), fg='red')
     partial_objective = make_reduce(options)(losses)
     objective += options.get('weight', 1) * partial_objective
   return objective
