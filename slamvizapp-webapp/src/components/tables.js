6ed52554b5154c570883021ba31027cd13f65078
Arthur Flam <arthur.flam@samsung.com>
2018-10-08 17:38:59 +0300

    tables now use smaller_is_better

--- slamvizapp-webapp/src/components/tables.js
+++ slamvizapp-webapp/src/components/tables.js
@@ -53,8 +53,9 @@ const ColumnsMetricImprovement = ({ metrics_new, metrics_ref, metric }) => {
     return <td style={{ background: "#bbb" }}>Ref missing</td>;
   let delta = metrics_new[metric.key] - metrics_ref[metric.key];
   let delta_relative = delta / (metrics_ref[metric.key] + 0.00001);
+  let quality = metric.smaller_is_better ? 0.5 - delta_relative : 0.5 + delta_relative;
   return (
-    <td style={{ background: interpolateRdYlGn(0.5 - delta_relative) }}>
+    <td style={{ background: interpolateRdYlGn(quality) }}>
       {metric_formatter.format(delta)} ({percent_formatter.format(
         100 * delta_relative
       )}%)
@@ -70,8 +71,8 @@ const QualityCell = ({ metric, metrics }) => {
   )
     return <td style={{ background: "#bbb" }}>na</td>;
   let value = metrics[metric.key];
-  const threshold = metric.target;
-  const quality = 0.5 + (threshold - value) / (threshold + 0.0001);
+  const delta_relative = (metric.target - value) / (metric.target + 0.0001);
+  let quality = metric.smaller_is_better ? 0.5 - delta_relative : 0.5 + delta_relative;
   return (
     <td style={{ background: interpolateRdYlGn(quality) }}>
       {metric_formatter.format(value)}
