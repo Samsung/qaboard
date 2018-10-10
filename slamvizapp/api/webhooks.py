a6fc822a42f17c87a9814ede0f1342ed35b478b8
Arthur Flam <arthur.flam@samsung.com>
2018-10-10 13:04:07 +0300

    fix is_failed support

--- slamvizapp/api/webhooks.py
+++ slamvizapp/api/webhooks.py
@@ -47,6 +47,7 @@ def update_batch():
   return "OK"
 
 
+
 @app.route('/api/v1/output', methods=['POST'])
 @app.route('/api/v1/output/', methods=['POST'])
 def new_output_webhook():
@@ -61,23 +62,23 @@ def new_output_webhook():
   try:
     ci_commit = CiCommit.get_or_create(
       session=db_session,
-      hexsha=request.json['git_commit_sha'],
-      project_id=request.json.get('project', 'dvs/psp_swip'),
+      hexsha=data['git_commit_sha'],
+      project_id=data.get('project', 'dvs/psp_swip'),
     )
   except:
-    return f"404 ERROR:\n there is an issue with your commit id ({request.json['git_commit_sha']})", 404
+    return f"404 ERROR:\n there is an issue with your commit id ({data['git_commit_sha']})", 404
 
   # The output belongs to this batch of outputs
   batch = ci_commit.get_or_create_batch(data['batch_label'])
 
   # We make sure the Test on which we ran exists in the database 
-  test_input_path = request.json.get('recording_path', request.json.get('input_path'))
+  test_input_path = data.get('recording_path', data.get('input_path'))
   if not test_input_path:
     return jsonify({"error": "the input path was not provided"}, 400)
   test_input = TestInput.get_or_create(
     db_session,
     path=test_input_path,
-    database=request.json.get('database', ci_commit.project.database),
+    database=data.get('database', ci_commit.project.database),
   )
   if not test_input: return "KO", 404
 
@@ -89,26 +90,28 @@ def new_output_webhook():
                                          extra_parameters=data['extra_parameters'],
                                          test_input=test_input,
                                         )
-  output.output_type = request.json.get('output_type', 'slam/6dof')
-  output.data = request.json.get('data', {})
+  output.output_type = data.get('output_type', 'slam/6dof')
+  output.data = data.get('data', {})
 
   # We allow users to save their data in custom locations
   # at the commit and output levels
-  if Path(request.json.get('commit_ci_dir', ci_commit.commit_dir)) != ci_commit.commit_dir:
-    ci_commit.commit_dir_override = request.json.get('commit_ci_dir')
-  if Path(request.json.get('output_directory', output.output_dir)) != output.output_dir:
-    output.output_dir_override = request.json.get('output_directory')
+  if Path(data.get('commit_ci_dir', ci_commit.commit_dir)) != ci_commit.commit_dir:
+    ci_commit.commit_dir_override = data.get('commit_ci_dir')
+  if Path(data.get('output_directory', output.output_dir)) != output.output_dir:
+    output.output_dir_override = data.get('output_directory')
+
+  output.is_failed = data.get('is_failed', False)
 
   # We update the output's status
-  output.is_running = request.json.get('is_running', False)
+  output.is_running = data.get('is_running', False)
   if output.is_running:
     output.is_pending = True
   else:
-    output.is_pending = request.json.get('is_pending', False)
+    output.is_pending = data.get('is_pending', False)
 
   # We save the output's metrics
   if not output.is_pending:
-    metrics = request.json.get('metrics', {})
+    metrics = data.get('metrics', {})
     if not metrics: # we look for metrics.json in the output directory
       output.update_metrics()
     else:
@@ -116,7 +119,6 @@ def new_output_webhook():
 
   db_session.add(output)
   db_session.commit()
-  print('-----------------')
   return "OK"
 
 
