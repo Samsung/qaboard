85b3b12912b5727f19d810bdbcdaeea105f1c620
Arthur Flam <arthur.flam@samsung.com>
2018-10-10 16:45:25 +0300

    missing space..

--- slamvizapp/api/tuning.py
+++ slamvizapp/api/tuning.py
@@ -198,7 +198,7 @@ def add_batch(hexsha):
         [
             "#!/bin/bash\n",
             f'bsub_su {data.get("user", "arthurf")} -q {queue} ',
-            '-W 24:00' if do_optimize else '-sp 4000 ', # highest priority for manual runs
+            '-W 24:00 ' if do_optimize else '-sp 4000 ', # highest priority for manual runs
             f'-o "{batch.output_dir}/log.txt" ',
             '<< "EOF"\n',
             f'  cd "{working_directory}";\n',
