eef43017d837879af6750056e5c982bda14e3fe5
Arthur Flam <arthur.flam@samsung.com>
2018-10-14 14:07:33 +0300

    remove warnings :mute:

--- slamvizapp-webapp/src/reducers/projects.js
+++ slamvizapp-webapp/src/reducers/projects.js
@@ -6,10 +6,9 @@ import {
   FETCH_COMMITS,
   UPDATE_COMMITS,
 } from '../actions/constants'
-import { default_project_id, default_project, slam_qatools_config, tof_qatools_config } from "../defaults"
+import { default_project_id, default_project, slam_qatools_config } from "../defaults"
 
 import * as slam_metrics from "../viewers/slam/metrics";
-import * as tof_metrics from "../viewers/tof/metrics";
 
 
 function update_project(state=default_project, data) {
