82b0f57eda022433f6488ac25a39413ea456885b
Arthur Flam <arthur.flam@samsung.com>
2018-11-29 12:01:49 +0200

    added image links

--- slamvizapp-webapp/src/viewers/image.js
+++ slamvizapp-webapp/src/viewers/image.js
@@ -9,17 +9,23 @@ class ImgViewer extends PureComponent {
   // }
   render() {
     const { path, output_new, output_ref } = this.props;
+    let new_url = `${output_new.output_dir_url}/${path}`
+    let ref_url = `${output_ref.output_dir_url}/${path}`
     return <div>
-      <img
-        alt="New"
-        src={`${output_new.output_dir_url}/${path}`}
-        width={400}
-      />
-      <img
-        alt="Reference"
-        src={`${output_ref.output_dir_url}/${path}`}
-        width={400}
-      />
+      <a href={new_url}>
+        <img
+          alt="New"
+          src={new_url}
+          width={400}
+        />
+      </a>
+      <a href={ref_url}>
+        <img
+          alt="Reference"
+          src={ref_url}
+          width={400}
+        />
+      </a>
     </div>
   }
 
