61cff201d8ac3feb03958c2bec28784ddc4d9530
Arthur Flam <arthur.flam@samsung.com>
2018-12-12 14:39:44 +0200

    no image smoothing?

--- slamvizapp-webapp/src/viewers/images.js
+++ slamvizapp-webapp/src/viewers/images.js
@@ -1,7 +1,7 @@
 import React, { PureComponent } from "react";
 import { get } from "axios"
 import { Tag } from "@blueprintjs/core";
-import OpenSeaDragon from 'openseadragon';
+import Openseadragon from 'openseadragon';
 import pixelmatch from 'pixelmatch';
 // import { Hotkey, Hotkeys, HotkeysTarget } from "@blueprintjs/core";
 
@@ -10,30 +10,33 @@ import "./image-canvas.css";
 
 
 
-// https://github.com/picturae/openseadragonrgb/blob/master/src/rgb.js
+// https://github.com/picturae/Openseadragonrgb/blob/master/src/rgb.js
 
-// http://openseadragon.github.io/docs/OpenSeadragon.html#.Options
-// http://openseadragon.github.io/docs/OpenSeadragon.Viewer.html
-// http://openseadragon.github.io/docs/OpenSeadragon.Viewport.html
-// http://openseadragon.github.io/#examples-and-features
-const openseadragon_config = {
+// http://Openseadragon.github.io/docs/Openseadragon.html#.Options
+// http://Openseadragon.github.io/docs/Openseadragon.Viewer.html
+// http://Openseadragon.github.io/docs/Openseadragon.Viewport.html
+// http://Openseadragon.github.io/#examples-and-features
+const Openseadragon_config = {
   visibilityRatio: 1,
 
-  minZoomLevel: 1,
-  defaultZoomLevel: 1,
   preserveViewport: true,
+  defaultZoomLevel: 1,
+  minZoomLevel: 1,
   maxZoomPixelRatio: 50,
+  minZoomImageRatio: 50,
   // smoothTileEdgesMinZoom: 10000,
+  imageSmoothingEnabled: false,
 
-  showNavigator: true,
-  sequenceMode: true,
   springStiffness: 15,
 
-  // for now, manually: cp -r node_modules/openseadragon/build/openseadragon /stage/algo_data/ci/
-  prefixUrl: "/s/stage/algo_data/ci/openseadragon/images/",
+  showNavigator: true,
+  // sequenceMode: true,
 
+  // for now, manually: cp -r node_modules/Openseadragon/build/Openseadragon /stage/algo_data/ci/
+  prefixUrl: "/s/stage/algo_data/ci/openseadragon/images/",
   crossOriginPolicy: 'Anonymous',
   ajaxWithCredentials: false,
+  // debugMode: true,
   // showReferenceStrip: false,
   // visibilityRatio: 1.0,
   // constrainDuringPan: false,
@@ -78,14 +81,15 @@ class ImgViewer extends PureComponent {
 
   componentDidMount() {
     const { output_new } = this.props;
-    let viewer_new = OpenSeaDragon({
-        ...openseadragon_config,
+    let viewer_new = Openseadragon({
+        ...Openseadragon_config,
         id: `osd-new-${output_new.output_dir_url}`,
       });
-    let viewer_ref = OpenSeaDragon({
-        ...openseadragon_config,
+    let viewer_ref = Openseadragon({
+        ...Openseadragon_config,
         id: `osd-ref-${output_new.output_dir_url}`,
     });
+
     this.setState({
       viewer_new,
       viewer_ref,
@@ -119,7 +123,7 @@ class ImgViewer extends PureComponent {
     const { viewer_new, viewer_ref} = this.state;
     if (this.props.diff) {
       var update_diff = () => {
-        let size_new = new OpenSeaDragon.Point(viewer_new.container.clientWidth || 1, viewer_new.container.clientHeight || 1);
+        let size_new = new Openseadragon.Point(viewer_new.container.clientWidth || 1, viewer_new.container.clientHeight || 1);
         let canvas_new = viewer_new.drawer.canvas
         let canvas_ref = viewer_ref.drawer.canvas
         let data_new = canvas_new.getContext("2d").getImageData(0, 0, size_new.x, size_new.y);
@@ -129,8 +133,8 @@ class ImgViewer extends PureComponent {
           var diff = canvas_diff.getContext("2d").createImageData(size_new.x, size_new.y);
           pixelmatch(data_new.data, data_ref.data, diff.data, size_new.x, size_new.y, {threshold: this.state.diff_threshold});
           canvas_diff.getContext("2d").putImageData(diff, 0, 0);          
-          // var point = new OpenSeaDragon.Point(0.5, 0.5)
-          // viewer_ref.addOverlay(`osd-diff-${output_new.output_dir_url}`, point, OpenSeaDragon.Placement.CENTER)
+          // var point = new Openseadragon.Point(0.5, 0.5)
+          // viewer_ref.addOverlay(`osd-diff-${output_new.output_dir_url}`, point, Openseadragon.Placement.CENTER)
         }
       }
       viewer_new.addHandler('animation-finish', update_diff);
@@ -152,6 +156,7 @@ class ImgViewer extends PureComponent {
         return;
       masterZoom = viewer_new.viewport.getZoom();
       masterCenter = viewer_new.viewport.getCenter();
+      if (masterCenter===undefined || masterCenter===null) return 
 
       viewer_newLeading = true;
       viewer_ref.viewport.zoomTo(masterZoom);
@@ -164,6 +169,7 @@ class ImgViewer extends PureComponent {
         return;
       masterZoom = viewer_ref.viewport.getZoom();
       masterCenter = viewer_ref.viewport.getCenter();
+      if (masterCenter===undefined || masterCenter===null) return
 
       viewer_refLeading = true;
       viewer_new.viewport.zoomTo(masterZoom);
@@ -176,8 +182,8 @@ class ImgViewer extends PureComponent {
     viewer_ref.addHandler('pan', viewer_refHandler);
 
     function maintainZoom() {
-        var size1 = new OpenSeaDragon.Point(viewer_new.container.clientWidth || 1, viewer_new.container.clientHeight || 1);
-        var size2 = new OpenSeaDragon.Point(viewer_ref.container.clientWidth || 1, viewer_ref.container.clientHeight || 1);
+        var size1 = new Openseadragon.Point(viewer_new.container.clientWidth || 1, viewer_new.container.clientHeight || 1);
+        var size2 = new Openseadragon.Point(viewer_ref.container.clientWidth || 1, viewer_ref.container.clientHeight || 1);
         viewer_newLeading = true;
         viewer_refLeading = true;
         
@@ -204,13 +210,13 @@ class ImgViewer extends PureComponent {
     const has_reference = !!output_new && output_new.output_dir_url;
 
     get(`${iiif_url(output_new.output_dir_url, path)}/info.json`).then(res => {
-      // https://openseadragon.github.io/examples/tilesource-iiif/
+      // https://Openseadragon.github.io/examples/tilesource-iiif/
       const { height, width } = res.data;
       let source_config = {
           "@context": "http://iiif.io/api/image/2/context.json",
           protocol: "http://iiif.io/api/image",
           profile: ["http://iiif.io/api/image/2/level2.json"],
-          formats: ["png"],
+          // formats: ["png"],
           fitBounds: true,
           height,
           width,
@@ -219,17 +225,55 @@ class ImgViewer extends PureComponent {
         width: this.state.width,
         height: `${parseFloat(this.state.width.replace(/[^\d]+/, '')) * height / width}px`,
       })
-      this.state.viewer_new.open([{
+
+      const { viewer_new, viewer_ref } = this.state;
+
+      viewer_new.open([{
             ...source_config,
             "@id": iiif_url(output_new.output_dir_url, path),
       }])
-
       if (has_reference) {
         this.state.viewer_ref.open([{
               ...source_config,
               "@id": iiif_url(output_ref.output_dir_url, path),
         }])
       }
+
+      // console.log(viewer_new.drawer.context.imageSmoothingEnabled)
+      viewer_new.drawer.setImageSmoothingEnabled(false);
+      viewer_ref.drawer.setImageSmoothingEnabled(false);
+      // viewer_new.drawer.context.imageSmoothingEnabled = false;
+      // viewer_ref.drawer.context.imageSmoothingEnabled = false;
+
+      // const context = v => v.drawer.canvas.getContext("2d")
+      // context(viewer_new).imageSmoothingEnabled = false;
+      // context(viewer_new).ImageSmoothingEnabled = false;
+      // context(viewer_new).webkitImageSmoothingEnabled = false;
+      // context(viewer_new).mozImageSmoothingEnabled = false;
+      // context(viewer_new).msImageSmoothingEnabled = false;
+
+      // context(viewer_ref).imageSmoothingEnabled = false;
+      // context(viewer_ref).ImageSmoothingEnabled = false;
+      // context(viewer_ref).webkitImageSmoothingEnabled = false;
+      // context(viewer_ref).mozImageSmoothingEnabled = false;
+      // context(viewer_ref).msImageSmoothingEnabled = false;
+
+      // let canvas_new = viewer_new.canvas.childNodes[0];
+      // let ctx_new = canvas_new.getContext('2d');
+      // ctx_new.mozImageSmoothingEnabled = false;
+      // ctx_new.webkitImageSmoothingEnabled = false;
+      // ctx_new.msImageSmoothingEnabled = false;
+      // ctx_new.imageSmoothingEnabled = false;
+
+      // let canvas_ref = viewer_ref.canvas.childNodes[0];
+      // let ctx_ref = canvas_ref.getContext('2d');
+      // ctx_ref.mozImageSmoothingEnabled = false;
+      // ctx_ref.webkitImageSmoothingEnabled = false;
+      // ctx_ref.msImageSmoothingEnabled = false;
+      // ctx_ref.imageSmoothingEnabled = false;
+
+
+
     }).catch(err => console.log(err));
   }
 
@@ -239,7 +283,7 @@ class ImgViewer extends PureComponent {
     return <div >
       <Tag intent={shown_image === "Reference" ? "primary" : "warning"} id="current_image">{shown_image}</Tag>
       <div style={{width, height}} id={`osd-new-${output_new.output_dir_url}`} />
-      {!!output_ref && !!output_ref.output_dir_url && <div style={{width, height}} id={`osd-ref-${output_new.output_dir_url}`} />}
+      <div hidden={!!!output_ref || !!!output_ref.output_dir_url} style={{width, height}} id={`osd-ref-${output_new.output_dir_url}`} />
       {this.props.diff && !!output_ref && !!output_ref.output_dir_url && <div style={{width, height}}>
         <canvas width={width} height={height} id={`osd-diff-${output_new.output_dir_url}`} />
       </div>}      
@@ -291,38 +335,38 @@ class ImgViewer extends PureComponent {
 
 
 
-        // https://github.com/openseadragon/openseadragon/issues/1376
-        // var tileLoadedHandler = function(eventSource, item, user) {
-        //     viewer_new.removeHandler('tile-loaded', tileLoadedHandler);
-        //     var imageBounds =viewer_new.world.getItemAt(0).getBounds();
-        //     viewer_new.viewport.fitBounds(imageBounds, true);
-        // };
-        // viewer_new.addHandler('tile-loaded', tileLoadedHandler);
-        // viewer_new.addHandler('full-screen', function(a){
-        //     console.log('full-screen-new')
-        //     console.log(a)
-        //     if(a.fullScreen == true) {
-        //         viewer_new.autoResize = true;
-        //     } else {
-        //         setTimeout(function () {
-        //             window.addEventListener('resize', maintainZoom);
-        //             viewer_new.autoResize = false;
-        //         }, 400);
-        //     }
-        // });
-        // viewer_ref.addHandler('full-screen',function(a){
-        //     if(a.fullScreen == true){
-        //         viewer_ref.autoResize = true;
-        //     } else {
-        //         setTimeout(function () {
-        //             window.addEventListener('resize', maintainZoom);
-        //             viewer_ref.autoResize = false;
-        //         }, 400);
-        //     }
-        // })
+// https://github.com/Openseadragon/Openseadragon/issues/1376
+// var tileLoadedHandler = function(eventSource, item, user) {
+//     viewer_new.removeHandler('tile-loaded', tileLoadedHandler);
+//     var imageBounds =viewer_new.world.getItemAt(0).getBounds();
+//     viewer_new.viewport.fitBounds(imageBounds, true);
+// };
+// viewer_new.addHandler('tile-loaded', tileLoadedHandler);
+// viewer_new.addHandler('full-screen', function(a){
+//     console.log('full-screen-new')
+//     console.log(a)
+//     if(a.fullScreen == true) {
+//         viewer_new.autoResize = true;
+//     } else {
+//         setTimeout(function () {
+//             window.addEventListener('resize', maintainZoom);
+//             viewer_new.autoResize = false;
+//         }, 400);
+//     }
+// });
+// viewer_ref.addHandler('full-screen',function(a){
+//     if(a.fullScreen == true){
+//         viewer_ref.autoResize = true;
+//     } else {
+//         setTimeout(function () {
+//             window.addEventListener('resize', maintainZoom);
+//             viewer_ref.autoResize = false;
+//         }, 400);
+//     }
+// })
 
-        // viewer.addHandler("page", data => {
-        //   this.setState({shown_image: data.page===0 ? "New" : "Reference"})
-        // });
+// viewer.addHandler("page", data => {
+//   this.setState({shown_image: data.page===0 ? "New" : "Reference"})
+// });
 
 export default ImgViewer;
