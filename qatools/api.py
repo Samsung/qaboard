f83a604c3dfd48cde286f7f65474978ca9bce233
Arthur Flam <arthur.flam@samsung.com>
2018-12-26 12:35:16 +0200

    removed comment

--- qatools/api.py
+++ qatools/api.py
@@ -28,7 +28,7 @@ class NumpyEncoder(json.JSONEncoder):
         elif isinstance(obj, (np.float_, np.float16, np.float32,
             np.float64)):
             return float(obj)
-        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+        elif isinstance(obj,(np.ndarray,)):
             return obj.tolist()
         return json.JSONEncoder.default(self, obj)
 
