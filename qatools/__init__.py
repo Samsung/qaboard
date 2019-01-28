1d6efe1c485127f0347e07dfe9b25b5c411dfb45
Arthur Flam <arthur.flam@samsung.com>
2019-01-28 10:49:26 +0200

    export slugify, and define it as Gitlab

--- qatools/__init__.py
+++ qatools/__init__.py
@@ -1,3 +1,4 @@
 from .config import on_windows, on_linux, on_lsf, on_vdi, is_ci
 from .config import config, merge
+from .utils import slugify
 from .qatools import cli
--- qatools/utils.py
+++ qatools/utils.py
@@ -1,22 +1,25 @@
 """
-Utilities related to CI: contacting the results database, naming conventions... 
+Misc utilities useful for qatools 
 """
 import os
+import re
 import json
 import yaml
-from pathlib import Path
-import hashlib
-import re
 import fnmatch
+import hashlib
+from pathlib import Path
 
 import click
-from qatools.config import is_ci, subproject
+
+from .conventions import make_hash, make_pretty_tuning_filename
+
 
 class PathType(click.ParamType):
   """Wrapper for pathlib's Path type, for use with the Click CLI package."""
   name = 'path'
   def convert(self, value, param, ctx):
     return Path(value)
+
 
 def file_info(path):
   """Return metadata about a file."""
@@ -34,6 +37,7 @@ def file_info(path):
     "md5": md5.hexdigest(),
   }
 
+
 def latest_commit(repo, branch):
     """Returns the latest commit on a branch."""
     # FIXME: couldn't we just use the project's git repo URL from the configuration?
@@ -42,42 +46,22 @@ def latest_commit(repo, branch):
     return list(repo.iter_commits(branch, max_count=1))[0]
 
 
-def slugify(s):
-  s_slugified = s
-  for c in ' /': # baaaaad
-    s_slugified = s_slugified.replace(c, '-')
-  return s_slugified
-
-def make_hash(obj):
-  params_s = json.dumps(obj, sort_keys=True)
-  return hashlib.md5(params_s.encode()).hexdigest()
-
-
-def batch_dir(commit_ci_dir, batch_label, tuning):
-  batch_folder = Path('output') if batch_label == 'default' else Path('tuning') / slugify(batch_label)
-  return commit_ci_dir / batch_folder if is_ci else subproject / batch_folder
+def slugify(s : str):
+  """Slugiy a string like they do at Gitlab."""
+  # lowercased and shortened to 63 bytes
+  slug = s.lower()[:63]
+  # everything except 0-9 and a-z replaced with -. 
+  slug = re.sub('[^0-9a-z]', '-', slug)
+  # No leading / trailing -. 
+  return slug.strip('-')
 
 
-def make_prefix_outputs_path(commit_ci_dir, batch_label, platform, configuration, tuning):
-  return (
-    batch_dir(commit_ci_dir, batch_label, tuning) /
-    platform /
-    # safer on windows
-    configuration.replace("/", '.') /
-    tuning_foldername(batch_label, hash_parameters(tuning))
-  )
-
-
-def tuning_foldername(batch_label, tuning_parameters_hash):
-  if batch_label != 'default':
-    if not tuning_parameters_hash:
-      param_hash = make_hash({})
-    else:
-      param_hash = tuning_parameters_hash
-    parameters_folder = Path(param_hash[:2]) / param_hash
-  else:
-    parameters_folder = ''
-  return parameters_folder 
+def getenvs(variables, default=None):
+  """Return the value of the environment variable that is defined - or None."""
+  for name in variables:
+    if name in os.environ:
+      return os.environ[name]
+  return default
 
 
 def load_tuning_search(tuning_search, tuning_search_file):
@@ -105,23 +89,6 @@ def load_tuning_search(tuning_search, tuning_search_file):
     filetype = 'json' # we default to json
   return tuning_search_dict, filetype
 
-def hash_parameters(parameters):
-  # we can specify either None, directly parameters, or a Path
-  if not parameters:
-    params = {}
-  elif isinstance(parameters, dict):
-    params = parameters
-  else:
-    with parameters.open('r') as f:
-      if parameters.suffix == '.yaml':
-        params = yaml.load(f)
-      elif parameters.suffix == '.cde':
-        from cde import Config
-        params = Config.loads(f.read()).asdict()
-      else:
-        params = json.load(f)
-  return make_hash(params)
-
 
 def iter_recordings(groups, groups_file, database, default_configuration, config, globs=None, debug=False):
   """Returns an iterator over the (recording, configuration) from the selected groups
@@ -131,7 +98,11 @@ def iter_recordings(groups, groups_file, database, default_configuration, config
   - configuration, is none is specified
   """
   if not globs:
-    globs = config['inputs']['glob']
+    globs = config.get('inputs', {}).get('glob', [])
+    if not globs:
+      click.secho(f'WARNING: Could not find how to identify input tests.', fg='yellow', err=True)
+      click.secho(f'Consider adding to qatools.yaml somelike like:\n```\ninputs:\n  glob: *.hex\n```', fg='yellow', err=True, dim=True)
+
   if not isinstance(globs, tuple) and not isinstance(globs, list):
     globs = [globs]
 
@@ -186,20 +157,6 @@ def iter_recordings(groups, groups_file, database, default_configuration, config
         else:
           yield from set([(maybe_parent(f), location_configuration) for f in (database / location).rglob(glob)])
 
-
-
-
-def make_pretty_tuning_filename(paramstring, filetype, maxlen=20):
-  """Best effort attempt at making a human-readable name from tuning parameters"""
-  thishash = make_hash(paramstring)
-  params_filename = paramstring.replace(",","_")
-  for char in "{}:[] \r\n\"":
-    params_filename = params_filename.replace(char,"")
-  if len(params_filename) > maxlen:
-    params_filename = thishash[:8] + '-' + re.sub("[a-zA-Z_]+", lambda x: x.group(0)[-2:], params_filename)
-  if len(params_filename) > maxlen:
-    params_filename = params_filename[-10:] + '-' + thishash[:10]
-  return f"{params_filename}.{filetype}"
 
 
 def iter_parameters(tuning_search=None, filetype='json', extra_parameters=None):
