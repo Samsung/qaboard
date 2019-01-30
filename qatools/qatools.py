23ebf6c52940db02644afdc2f643c6c21fc6b037
Arthur Flam <arthur.flam@samsung.com>
2019-01-30 18:00:34 +0200

    less verbose

--- qatools/qatools.py
+++ qatools/qatools.py
@@ -558,7 +558,7 @@ def check_bit_accuracy(ctx, reference, group, groups_file):
         prefix_output_dir = make_prefix_outputs_path(Path(), ctx.obj['batch_label'], ctx.obj["platform"], serialize_config(input_configurations), None, ctx.obj['ci'])
         input_path = input_path_abs.relative_to(ctx.obj['database'])
         output_directory = prefix_output_dir / input_path.with_suffix('')
-        print(output_directory)
+        # print(output_directory)
         output_directories.append(output_directory)
 
 
