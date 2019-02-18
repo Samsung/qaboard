398d98a2b392c8e1e14d64066966f51c7350e034
Arthur Flam <arthur.flam@samsung.com>
2019-02-18 14:12:35 +0200

    Dont fallback on the application for /api and /s routes: those should be served from the API

--- slamvizapp-webapp/config-overrides.js
+++ slamvizapp-webapp/config-overrides.js
@@ -5,9 +5,23 @@ module.exports = function override(config, env) {
   if (!config.plugins) {
     config.plugins = [];
   }
+
   config.plugins.push(
     new MonacoWebpackPlugin()
   );
+
+  // console.log(config.plugins)
+  // the names of the plugins could change in the future..
+  // you debug the changes easily with console.log statements
+  config.plugins.forEach(plugin => {
+    if (plugin.constructor.name === "GenerateSW") {
+      plugin.config.navigateFallbackBlacklist = [
+        /^\/s\/.*/,
+        /^\/api\/.*/,
+      ];
+      // console.log(plugin.config)
+    }
+  });
+  // console.log(config.plugins.filter(plugin => plugin.constructor.name === "GenerateSW"))
   return config;
 }
-
