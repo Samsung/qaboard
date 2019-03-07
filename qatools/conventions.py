812878a2cbef53e885c603ce34f52e519bc12e0d
Arthur Flam <arthur.flam@samsung.com>
2019-03-07 10:31:07 +0200

    avoid issues when tuning params contain a slash

--- qatools/conventions.py
+++ qatools/conventions.py
@@ -66,7 +66,7 @@ def make_pretty_tuning_filename(paramstring, filetype, maxlen=20):
   """Best effort attempt at making a human-readable name from tuning parameters"""
   thishash = make_hash(paramstring)
   params_filename = paramstring.replace(",","_")
-  for char in "{}:[] \r\n\"":
+  for char in "{}:[] \r\n\"/":
     params_filename = params_filename.replace(char,"")
   if len(params_filename) > maxlen:
     params_filename = thishash[:8] + '-' + re.sub("[a-zA-Z_]+", lambda x: x.group(0)[-2:], params_filename)
