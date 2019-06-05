cd0d3728e04bb7ae8696a338ca1bcbf95fc30c48
Arthur Flam <arthur.flam@samsung.com>
2019-06-05 14:29:43 +0300

    Fix the URL param we read the batch from

--- slamvizapp-webapp/src/defaults.js
+++ slamvizapp-webapp/src/defaults.js
@@ -102,7 +102,7 @@ export const default_selected = () => {
 	ref_commit_id: params.get("reference") || params.get("commit_ref_folder") || null,
 
     // What batch of results should we show, with what filters?
-	selected_batch_new: params.get("batch") || "default",
+	selected_batch_new: params.get("batch") || params.get("batch_new") || "default",
 	selected_batch_ref: params.get("batch_ref") || "default",
 
 	filter_batch_new: params.get("filter") || "",
