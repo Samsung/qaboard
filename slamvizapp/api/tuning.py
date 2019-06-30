08d6d5713ee5beee5718c0dfc2b99b21f62a18a9
Arthur Flam <arthur.flam@samsung.com>
2019-06-30 13:31:45 +0300

    Add missing "source envrc" when working with subprojects

--- slamvizapp/api/tuning.py
+++ slamvizapp/api/tuning.py
@@ -201,12 +201,13 @@ def add_batch(hexsha):
     # We could also play with heredocs-within-heredocs, but it is painful, and this way we get logs
     # openstf is our Android device farm
     use_openstf = data["android_device"].lower() == "openstf"
+    envrcs = [f"source {p}/.envrc\n" for p in list(reversed(list(working_directory.parents))) if (p / '.envrc').exists()]
     qa_batch_script = "".join(
         [
             "#!/bin/bash\n",
             "set -xe\n\n",
             f'cd "{working_directory}";\n\n',
-            "source .envrc\n\n" if (working_directory / '.envrc').exists() else "",
+            ('\n'.join(envrcs) + '\n') if envrcs else "",
             # qa uses click, which hates non-utf8 locales
             'export LC_ALL=en_US.utf8;\n',
             'export LANG=en_US.utf8;\n\n',
