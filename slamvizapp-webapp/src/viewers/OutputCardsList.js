a1b19b2f08382f23df4edb5e597780797c4cf23c
Arthur Flam <arthur.flam@samsung.com>
2019-08-06 13:54:05 +0300

    Pass props down

--- slamvizapp-webapp/src/viewers/OutputCardsList.js
+++ slamvizapp-webapp/src/viewers/OutputCardsList.js
@@ -121,6 +121,7 @@ class OutputCardsList extends React.Component {
                   output_new={output}
                   output_ref={ref_batch.outputs[output.reference_id]}
                   warning={output.reference_warning}
+                  dispatch={this.props.dispatch}
                   {...misc_output_props}
                 />
               );
