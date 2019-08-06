c4ad6c1994a91a2527ffc5b4e2436b95d9e70843
Arthur Flam <arthur.flam@samsung.com>
2019-08-06 18:16:16 +0300

    more verbose

--- qatools/api.py
+++ qatools/api.py
@@ -97,13 +97,13 @@ def notify_qa_database(object_type='output', **kwargs):
     # we can't use requests' json serialization (simplejson or json) because it fails with numpy arrays
     data = simplejson.dumps(data, ignore_nan=True, cls=NumpyEncoder)
     r = requests.post(url, data=data, headers={'Content-Type': 'application/json'})
+    if 'QATOOLS_VERBOSE' in os.environ:
+      click.secho(r.text, fg='cyan', dim=True, err=True)
     r.raise_for_status()
     try:
       data = r.json()
-      if 'QATOOLS_VERBOSE' in os.environ:
-        print(data)
     except:
-      pass
+      print(f"WARNING: can't read the response!: {r.text}")
   except:
     click.secho('WARNING: Failed to update the QA database.', fg='yellow', err=True)
     click.secho(url, fg='yellow', err=True)
