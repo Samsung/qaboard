e3c22278381e435bc9d7a541103953d423da96e8
Arthur Flam <arthur.flam@samsung.com>
2019-08-18 13:56:26 +0300

    Refactored the backend support for milestones

--- slamvizapp/api/milestones.py
+++ slamvizapp/api/milestones.py
@@ -1,70 +1,38 @@
 """
-Save Load and Remove milestones of batches in DB
+CRUD operations for project milestones.
 """
-
-from flask import request, jsonify
+from flask import request
 from slamvizapp import app, db_session
 from sqlalchemy.orm.attributes import flag_modified
+
 from ..models import Project
 
-'''
-@app.route("/api/v1/project/milestones", methods=['GET', 'POST', 'DELETE', 'PUT'])
+
+@app.route("/api/v1/project/milestones", methods=['GET', 'POST', 'DELETE'])
+@app.route("/api/v1/project/milestones/", methods=['GET', 'POST', 'DELETE'])
 def crud_milestones():
-  if method=='GET':
-    pass
-  if method=='POST':
-    pass
-  if method=='PUT':
-    pass
-'''
-
-
-@app.route("/api/v1/project/milestones/get", methods=['GET'])
-def get_milestones():
-  project_id = request.args.get('project')
+  project_id = request.args['project']
   try:
     project = Project.query.filter(Project.id == project_id).one()
   except:
-    return 'Not found', 404
-  if 'milestones' in project.data:
-    return project.data['milestones']
+    return f'ERROR: Project not found', 404
+  milestones = project.data.get('milestones', {})
+  
+  if method == 'GET':
+    return milestones
+
+  data = request.get_json()
+  # The body for HTTP DELETE requests can be dropped by proxies (eg uwsgi, nginx...)
+  # so it's simpler to reuse the POST method...
+  if method=='DELETE' or data['delete']:
+    milestones[data['key']] = data['milestone']
   else:
-    return {}
+    milestones = project.data['milestones']
+    del milestones[data['key']]
 
-
-@app.route("/api/v1/project/milestones/save", methods=[ 'POST'])
-def save_milestone():
-  data = request.get_json()
-  project = Project.query.filter(Project.id == data['project']).one()
-
-  milestones = project.data['milestones']
-  key = data['key']
-  data.pop('project','key') # remove items we don't need to save.
-  milestones[key] = data
-  project.data.update({'milestones': milestones})
-  #print("milestones: ", project.data['milestones'])
-
+  project.data['milestones'] = milestones
   flag_modified(project, "data")
   db_session.add(project)
   db_session.commit()
-
-  return project.data['milestones']
-
-
-@app.route("/api/v1/project/milestones/remove", methods=['POST'])
-def remove_milestone():
-  data = request.get_json()
-  project = Project.query.filter(Project.id == data['project']).one()
-
-  milestones = project.data['milestones']
-
-  key = data['key']
-  milestones.pop(key)
-
-  project.data.update({'milestones': milestones})
-
-  db_session.add(project)
-  flag_modified(project, "data")
-  db_session.commit()
-
+  print(f"UPDATE: Milestones {project_id}: {project.data['milestones']}")
   return project.data['milestones']
