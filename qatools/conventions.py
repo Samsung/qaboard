b4c7337489276afbc65d172aa34fc4d5c5447dff
Arthur Flam <arthur.flam@samsung.com>
2019-08-19 18:02:43 +0300

    Fixed extra quote

--- qatools/conventions.py
+++ qatools/conventions.py
@@ -18,7 +18,7 @@ def get_commit_ci_dir(ci_dir, commit):
   if isinstance(commit, str):
     try:
       p = subprocess.run(
-        ["git", "show", "-s", "--format='%at|%an|%H"],
+        ["git", "show", "-s", "--format=%at|%an|%H"],
         encoding='utf8',
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE
