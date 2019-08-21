01d46c38197d0b0f4eec7ec89c4de3e18bf74fdc
Arthur Flam <arthur.flam@samsung.com>
2019-08-21 15:24:53 +0300

    Fixed DELETE/POST switch

--- slamvizapp/api/milestones.py
+++ slamvizapp/api/milestones.py
@@ -1,7 +1,7 @@
 """
 CRUD operations for project milestones.
 """
-from flask import request
+from flask import request, jsonify
 from slamvizapp import app, db_session
 from sqlalchemy.orm.attributes import flag_modified
 
@@ -11,28 +11,27 @@ from ..models import Project
 @app.route("/api/v1/project/milestones", methods=['GET', 'POST', 'DELETE'])
 @app.route("/api/v1/project/milestones/", methods=['GET', 'POST', 'DELETE'])
 def crud_milestones():
-  project_id = request.args['project']
+  data = request.get_json()
+  project_id = data['project']
   try:
     project = Project.query.filter(Project.id == project_id).one()
   except:
     return f'ERROR: Project not found', 404
   milestones = project.data.get('milestones', {})
   
-  if method == 'GET':
-    return milestones
+  if request.method == 'GET':
+    return jsonify(milestones)
 
-  data = request.get_json()
   # The body for HTTP DELETE requests can be dropped by proxies (eg uwsgi, nginx...)
   # so it's simpler to reuse the POST method...
-  if method=='DELETE' or data.get('delete')=='true':
-    milestones[data['key']] = data['milestone']
-  else:
-    milestones = project.data['milestones']
+  if request.method=='DELETE' or data.get('delete') in ['true', True]:
     del milestones[data['key']]
+  else:
+    milestones[data['key']] = data['milestone']
 
   project.data['milestones'] = milestones
   flag_modified(project, "data")
   db_session.add(project)
   db_session.commit()
   print(f"UPDATE: Milestones {project_id}: {project.data['milestones']}")
-  return project.data['milestones']
+  return jsonify(project.data['milestones'])
