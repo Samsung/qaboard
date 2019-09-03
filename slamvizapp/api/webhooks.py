3f85650226cf76a90dd8923bff51c728dd3d71de
Arthur Flam <arthur.flam@samsung.com>
2019-09-03 15:31:51 +0300

    always return JSON

--- slamvizapp/api/webhooks.py
+++ slamvizapp/api/webhooks.py
@@ -128,10 +128,7 @@ def new_output_webhook():
       project_id=data['project'],
     )
   except:
-    if is_ci:
-      return f"404 ERROR:\n Could not find your commit ({data['git_commit_sha']}).", 404
-    else: # for now let's not break anything...
-      return f"OK"
+    return jsonify({"error": f"Could not find your commit ({data['git_commit_sha']})."}), 404
 
   ci_commit.project.latest_output_datetime = datetime.datetime.utcnow()
   ci_commit.latest_output_datetime = datetime.datetime.utcnow()
@@ -145,7 +142,6 @@ def new_output_webhook():
     path=test_input_path,
     database=data.get('database', ci_commit.project.database),
   )
-  if not test_input: return "KO", 404
 
   # We save the basic information about our result
   batch = ci_commit.get_or_create_batch(data['batch_label'])
