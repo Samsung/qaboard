b965ca2196312587b700666d6847c001c70c8939
Arthur Flam <arthur.flam@samsung.com>
2019-10-16 10:59:45 +0300

    Improved performance for gitlab webhooks

--- slamvizapp/api/webhooks.py
+++ slamvizapp/api/webhooks.py
@@ -8,15 +8,11 @@ import datetime
 
 from flask import request, jsonify
 from sqlalchemy.orm.exc import NoResultFound
-from sqlalchemy.orm.attributes import flag_modified
-
-# from qatools.config import merge
-import qatools
 
 from slamvizapp import app, repos, db_session
 from ..models import Project, CiCommit, Batch, Output, TestInput
-from ..git_utils import git_pull
-
+from ..models.Project import update_project
+from ..utils import profiled
 
 @app.route('/api/v1/batch/stop', methods=['POST'])
 @app.route('/api/v1/batch/stop/', methods=['POST'])
@@ -199,110 +195,8 @@ def gitlab_webhook():
   # https://docs.gitlab.com/ce/user/project/integrations/webhooks.html
   data = json.loads(request.data)
   print(data, file=sys.stderr)
-
-  branch = data['ref'][11:] # data['ref'] => 'refs/heads/feature/Imu_preintegration'
-
-  # Update the root project - all subprojects depend on it
-  root_project_id = data['project']['path_with_namespace'] # eg => dvs/psp_swip
-  root_project = Project.get_or_create(session=db_session, id=root_project_id)
-  root_project.data.update({'git': data['project']})
-  db_session.add(root_project)
-  # https://stackoverflow.com/questions/30088089/sqlalchemy-json-typedecorator-not-saving-correctly-issues-with-session-commit
-  flag_modified(root_project, "data")
-  db_session.commit()
-  repo = repos[root_project_id]
-  git_pull(repo)
-
-  # List all the files named "qatools.yaml" in this commit
-  repo_files = repo.git.ls_tree('--name-only', '-r', data['checkout_sha']).splitlines()
-  projects_config_paths = [Path(f) for f in repo_files if f.endswith('qatools.yaml')]
-  for subproject_config_path in projects_config_paths:
-    # Each one is a subproject
-    project_id = str(root_project_id  / subproject_config_path.parent)
-    # Make sure the it exists in the database, with up-to-date metadata
-    project = Project.get_or_create(session=db_session, id=project_id)
-    project.data.update({'git': data['project']})
-    flag_modified(project, "data")
-    db_session.add(project)
-    db_session.commit()
-    # print(project)
-
-    try:
-      ci_commit = CiCommit.get_or_create(
-        session=db_session,
-        hexsha=data['checkout_sha'],
-        project_id=project_id,
-      )
-    except Exception as e:
-      exc_type, exc_value, exc_traceback = sys.exc_info()
-      info = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-      print(info, file=sys.stderr)
-      return f"404 ERROR: with commit id {data['checkout_sha']} in project {project_id}: {info}", 404
-
-    # To update the (sub)project configuration stored in the database,
-    # we first need to read relevant qatools.yaml files from this commit.
-    def is_relative_to(path, path_maybe_parent):
-      try:
-        relative_path = path.relative_to(path_maybe_parent)
-        return True
-      except:
-        return False
-    config_paths = [p for p in projects_config_paths if is_relative_to(subproject_config_path.parent, p.parent)]
-    config_paths.sort()
-    try:
-      configs_contents = [repo.git.show(f'{ci_commit.hexsha}:{p}') for p in config_paths]
-      configs = [yaml.load(c) for c in configs_contents]
-      qatools_config = {}
-      for c in configs:
-        qatools_config = qatools.merge(c, qatools_config)
-      qatools_config['project']['name'] = project_id
-    except Exception as e:
-      exc_type, exc_value, exc_traceback = sys.exc_info()
-      info = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-      print(info, file=sys.stderr)
-      continue
-
-    # We store qatools's configuration twice: at the project level and at the commit level
-    # - Commit-level info is important to let users easily tweak the outputs and metrics
-    #   they want to see when working on their branches 
-    ci_commit.data.update({'qatools_config': qatools_config})
-    # - Project-level information is used as a default or when showing in the UI list of commits
-    #   It is only updated when there are changes on the "reference branch" (eg master, develop...)
-    #   This said, we also update project-level data when it's the first time we get a qatools config for a project
-    is_initialization = 'qatools_config' not in project.data
-    reference_branch = qatools_config['project'].get('reference_branch', 'master')
-    is_reference = branch == reference_branch
-    if is_initialization or is_reference:
-      project.data.update({'qatools_config': qatools_config,})
-      flag_modified(project, "data")
-
-    metrics_path = qatools_config.get('outputs', {}).get('metrics')
-    if metrics_path:
-      try:
-        metrics_content = repo.git.show('{}:{}'.format(ci_commit.hexsha, metrics_path))
-      except:
-        metrics_content = None
-      if metrics_content:
-        try:
-          if metrics_path.endswith('yaml'):
-              metrics = yaml.load(metrics_content)        
-          elif metrics_path.endswith('json'):
-            metrics = json.loads(metrics_content)
-          ci_commit.data.update({'qatools_metrics': metrics})
-          flag_modified(ci_commit, "data")
-          if is_initialization or is_reference:
-            project.data.update({'qatools_metrics': metrics})
-            flag_modified(project, "data")
-        except Exception as e: 
-          exc_type, exc_value, exc_traceback = sys.exc_info()
-          info = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
-          print(info, file=sys.stderr)
-
-    print('project.data :', project.data)
-    db_session.add(ci_commit)
-    db_session.add(project)
-    db_session.commit()
-
+  with profiled():
+    update_project(data, db_session)
   return "{status:'OK'}"
 
 
--- slamvizapp/models/Project.py
+++ slamvizapp/models/Project.py
@@ -1,18 +1,27 @@
 """
 Describes a project
 """
+import sys
+import json
+import yaml
+import traceback
 from pathlib import Path
+from functools import lru_cache
 
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, ForeignKey
 from sqlalchemy import String, DateTime, JSON
 from sqlalchemy import cast, type_coerce
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.attributes import flag_modified
+
+import qatools
 
 # "from X import Y" can cause circular import errors..
 from slamvizapp.models import Base, CiCommit  
 # import slamvizapp.models as models
 from slamvizapp import repos
+from ..git_utils import git_pull
 from ..config import default_ci_directory
 
 class Project(Base):
@@ -87,3 +96,117 @@ class Project(Base):
 
   def __repr__(self):
     return f"<Project id='{self.id}' repo='{self.id_git}' subproject='{self.id_relative}'>"
+
+
+
+
+
+def is_relative_to(path : Path, path_maybe_parent : Path) -> bool:
+  try:
+    relative_path = path.relative_to(path_maybe_parent)
+    return True
+  except:
+    return False
+
+
+def update_project_data(project, data, db_session):
+  project.data.update({'git': data['project']})
+  db_session.add(project)
+  # https://stackoverflow.com/questions/30088089/sqlalchemy-json-typedecorator-not-saving-correctly-issues-with-session-commit
+  flag_modified(project, "data")
+  db_session.commit()
+
+
+
+
+def update_project(data, db_session):
+  # TODO: refactor, call the logic in Commit.get_or_create
+  branch = data['ref'][11:] # data['ref'] => 'refs/heads/feature/Imu_preintegration'
+  commit_id = data['checkout_sha']
+
+  # Update the root project - all subprojects depend on it
+  root_project_id = data['project']['path_with_namespace'] # eg => dvs/psp_swip
+  root_project = Project.get_or_create(session=db_session, id=root_project_id)
+  update_project_data(root_project, data, db_session)
+
+  repo = repos[root_project_id]
+  git_pull(repo)
+
+  @lru_cache(maxsize=128)
+  def parsed_content(commit_id, path):
+    """Read and parse a file in the git repository"""
+    try:
+      content = repo.git.show(f'{commit_id}:{path}')
+      if not content:
+        return None
+      if str(path).endswith('yaml'):
+          return yaml.load(content)        
+      elif str(path).endswith('json'):
+        return json.loads(content)
+      return yaml.load(content)
+    except Exception as e:
+      exc_type, exc_value, exc_traceback = sys.exc_info()
+      info = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+      print(info, file=sys.stderr)
+      return None
+
+
+  # List all the files named "qatools.yaml" in this commit
+  repo_files = repo.git.ls_tree('--name-only', '-r', commit_id).splitlines()
+  projects_config_paths = [Path(f) for f in repo_files if f.endswith('qatools.yaml')]
+  for subproject_config_path in projects_config_paths:
+    project_id = str(root_project_id / subproject_config_path.parent)
+    # Make sure the it exists in the database, with up-to-date metadata
+    project = Project.get_or_create(session=db_session, id=project_id)
+    update_project_data(project, data, db_session)
+    try:
+      ci_commit = CiCommit.get_or_create(
+        session=db_session,
+        hexsha=commit_id,
+        project_id=project_id,
+      )
+    except Exception as e:
+      exc_type, exc_value, exc_traceback = sys.exc_info()
+      info = ''.join(traceback.format_exception(exc_type, exc_value, exc_traceback))
+      print(info, file=sys.stderr)
+      return f"404 ERROR: with commit id {commit_id} in project {project_id}: {info}", 404
+
+    # To update the (sub)project configuration stored in the database,
+    # we first need to read relevant qatools.yaml files from this commit.
+    config_paths = [p for p in projects_config_paths if is_relative_to(subproject_config_path.parent, p.parent)]
+    config_paths.sort()
+
+    qatools_config = {}
+    for config_path in config_paths:
+      config = parsed_content(commit_id, config_path)
+      qatools_config = qatools.merge(config, qatools_config)
+    qatools_config['project']['name'] = project_id
+
+    # We store qatools's configuration twice: at the project level and at the commit level
+    # - Commit-level info is important to let users easily tweak the outputs and metrics
+    #   they want to see when working on their branches 
+    ci_commit.data.update({'qatools_config': qatools_config})
+    # - Project-level information is used as a default or when showing in the UI list of commits
+    #   It is only updated when there are changes on the "reference branch" (eg master, develop...)
+    #   This said, we also update project-level data when it's the first time we get a qatools config for a project
+    is_initialization = 'qatools_config' not in project.data
+    reference_branch = qatools_config['project'].get('reference_branch', 'master')
+    is_reference = branch == reference_branch
+    if is_initialization or is_reference:
+      project.data.update({'qatools_config': qatools_config,})
+      flag_modified(project, "data")
+
+    metrics_path = qatools_config.get('outputs', {}).get('metrics')
+    if metrics_path:
+      metrics = parsed_content(commit_id, metrics_path)
+      if metrics:
+        ci_commit.data.update({'qatools_metrics': metrics})
+        flag_modified(ci_commit, "data")
+        if is_initialization or is_reference:
+          project.data.update({'qatools_metrics': metrics})
+          flag_modified(project, "data")
+
+    # print('project.data :', project.data)
+    db_session.add(ci_commit)
+    db_session.add(project)
+    db_session.commit()
