9ead3fcd412da760074d1018914ebd3c60ec20cc
Arthur Flam <arthur.flam@samsung.com>
2019-10-30 19:22:18 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     'flask_cors',
     'sqlalchemy',       # ORM
     'alembic',          # SQL schema migrations
-    'psycopg2-binary',  # postgresql driver used by sqlalchemy
+    'psycopg2',  # postgresql driver used by sqlalchemy
     'sqlalchemy_utils',
     'flask-admin',
     'uwsgi', # actually required, see below
