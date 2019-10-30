2079a30b28e3498cfed90aa35b7e48624b0c8ca9
Arthur Flam <arthur.flam@samsung.com>
2019-10-30 17:01:34 +0200

    Support config with IP

--- slamvizapp-webapp/src/viewers/images/images.js
+++ slamvizapp-webapp/src/viewers/images/images.js
@@ -60,8 +60,8 @@ const iiif_url = (output_dir_url, path) => {
   identifier = `${identifier}/${path}`;
   // IIIF specs require encoding the slashes inside the identifier
   let is_cde_file = identifier.endsWith('dng') || identifier.endsWith('raw') || identifier.endsWith('hex')
-  let endpoint = is_cde_file ? 'https://qa:8186/fcgi-bin/iipsrv.fcgi?IIIF='
-    : 'https://qa:8183/iiif/2/'
+  let endpoint = is_cde_file ? `${window.location.protocol}//${window.location.hostname}:8186/fcgi-bin/iipsrv.fcgi?IIIF=`
+    : `${window.location.protocol}//${window.location.hostname}:8183/iiif/2/`
   identifier = encodeURIComponent(identifier)
   let url = `${endpoint}${identifier}`
   return url
