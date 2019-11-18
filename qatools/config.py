69b6ebb55dec65ffa22f5a5f1673990c3316cbdb
Arthur Flam <arthur.flam@samsung.com>
2019-11-18 11:21:09 +0200

    More proeminent warnings

--- qatools/config.py
+++ qatools/config.py
@@ -36,12 +36,12 @@ renamings = (
 def renamed_deprecated(arg):
   for before, after in renamings:
     if arg == before:
-      click.secho(f'Deprecation: {before} was replaced by {after} and will be removed in a future release.', fg='yellow')
+      click.secho(f'DEPRECATION WARNING: "{before}" was replaced by "{after}" and will be removed in a future release.', fg='yellow')
       return after
   return arg
 sys.argv = [renamed_deprecated(arg) for arg in sys.argv]
 if '--lsf-sequential' in sys.argv:
-  click.secho('WARNING: --lsf-sequential was replaced with `--runner local`', fg='yellow', bold=True)
+  click.secho('DEPRECATION WARNING: "--lsf-sequential" was replaced with "--runner local"', fg='yellow', bold=True)
 
 
 def find_qatools_configs(path):
