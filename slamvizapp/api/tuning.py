a810f71fb4ddc284ee515fcd958735cb98904b79
Arthur Flam <arthur.flam@samsung.com>
2019-11-19 10:34:42 +0200

    Rename --group to --batch

--- slamvizapp/api/tuning.py
+++ slamvizapp/api/tuning.py
@@ -74,7 +74,9 @@ def get_commit_groups_paths(project, commit_id):
     ci_commit = CiCommit.query.filter(
         CiCommit.project_id == project.id, CiCommit.hexsha.startswith(commit_id)
     ).one()
-    commit_group_files = ci_commit.data['qatools_config'].get('inputs', {}).get('groups', [])
+    commit_config_inputs = ci_commit.data['qatools_config'].get('inputs', {})
+    commit_group_files = commit_config_inputs.get('batches', commit_config_inputs.get('groups', []))
+    print(commit_group_files, file=sys.stderr)
     if not (isinstance(commit_group_files, list) or isinstance(commit_group_files, tuple)):
       commit_group_files = [commit_group_files]
 
@@ -89,6 +91,9 @@ def get_commit_groups_paths(project, commit_id):
 
 @app.route("/api/v1/tests/group")
 def get_group():
+    if not request.args["name"]:
+        return jsonify({"tests": []})
+
     project_id = request.args["project"]
     project = Project.get_or_create(session=db_session, id=project_id)
 
