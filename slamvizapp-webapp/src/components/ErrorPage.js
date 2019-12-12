43ea84beebdb08fda957131f5c58f31e3d7252dc
Arthur Flam <arthur.flam@samsung.com>
2019-12-12 11:48:58 +0200

    Better error page

--- slamvizapp-webapp/src/components/ErrorPage.js
+++ slamvizapp-webapp/src/components/ErrorPage.js
@@ -7,11 +7,12 @@ class ErrorPage extends React.Component {
 	render() {
 		let subject = encodeURIComponent("[qa] bug report");
 		let error = this.props.error.toString()
-		let componentStack = JSON.stringify((this.props.info || {}).componentStack)
+
+		let componentStack = (this.props.info || {}).componentStack
 		let body = encodeURIComponent(`URL: ${document.URL}\nerror: ${error}\ncomponentStack: ${componentStack}`)
 		return <Container>
 			<Callout intent={Intent.DANGER} title="Sorry, something went wrong!">
-				<p>Try refreshing the page.</p>
+				<p>Try refreshing the page. If you're lucky, try the <a href={`http://qa:3000${window.location.pathname}`}>staging version</a></p>
 				<p><b>Point of contact:</b> Arthur Flam <span className={Classes.TEXT_MUTED}>(+972-(0)58-706-2016) WhatsApp/Phone </span></p>
 				<p><a href={`mailto:arthur.flam@samsung.com?subject=${subject}&body=${body}`}><Button>Report the bug</Button></a></p>
 				<p><code dangerouslySetInnerHTML={{ __html: error || "" }}></code></p>
