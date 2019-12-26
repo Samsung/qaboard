e5a15c8789be4c5f4b6726abee60f0b1cf89c0c7
Arthur Flam <arthur.flam@samsung.com>
2019-12-26 12:29:18 +0200

    Update utils.py

--- qatools/utils.py
+++ qatools/utils.py
@@ -198,6 +198,7 @@ def is_plaintext(path, config=None):
   binary_patterns = config.get('bit_accuracy', {}).get('binary')
   if binary_patterns: # remove when everybody updates HW_ALG...
     binary_patterns.append('.exe')
+    binary_patterns.append('.dll')
   #print("binary: ", binary_patterns)
   plaintext_patterns = config.get('bit_accuracy', {}).get('plaintext')
   #print("plaintext: ", plaintext_patterns)
