9d2f3695574a6b73c8e06d60e6f485e496c506af
Arthur Flam <arthur.flam@samsung.com>
2020-01-29 12:39:22 +0200

    Added tests

--- tests/test_conventions.py
+++ tests/test_conventions.py
@@ -6,18 +6,20 @@ import unittest
 
 
 class TestConventions(unittest.TestCase):
-  def test_serialize(self):
+  def test_serialize_config(self):
     from qatools.conventions import serialize_config
     self.assertEqual(serialize_config(['a', 'b', 'c']), 'a:b:c')
     self.assertEqual(serialize_config(['a', {'b': 1}]), 'a:{"b": 1}')
     self.assertEqual(serialize_config(['a', '{"b":1']), 'a:{"b":1')
 
-  def deserialize_config(self):
-    from qatools.conventions import deserialize
+  def test_deserialize_config(self):
+    from qatools.conventions import deserialize_config
     self.assertEqual(deserialize_config('a:b:c'), ['a', 'b', 'c'])
     self.assertEqual(deserialize_config('a:{"b":1}'), ['a', {'b': 1}])
     self.assertEqual(deserialize_config('a:{"b":1'), ['a', '{"b":1'])
-    self.assertEqual(deserialize_config('a:C://path:b'), ['a', 'C://path', 'b'])
+    # we do those adjustments only on windows...
+    # self.assertEqual(deserialize_config('a:C://path:b'), ['a', 'C://path', 'b'])
+    # self.assertEqual(deserialize_config('C://path:a'), ['C://path', 'a'])
 
 
 if __name__ == '__main__':
