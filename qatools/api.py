e8c35c49046afc027ebb8ea17238cc93f7fe62ac
Arthur Flam <arthur.flam@samsung.com>
2020-02-03 09:57:19 +0200

    Fix un-renamed variable

--- qatools/api.py
+++ qatools/api.py
@@ -33,7 +33,7 @@ def print_url(ctx, status="starting"):
   if not ctx.obj['offline']:
     from requests.utils import quote
     batch_label = ctx.obj["batch_label"]
-    commit_url = f"https://{api_host}/{config['project']['name']}/commit/{commit_id if commit_id else ''}{f'?batch={quote(batch_label)}' if batch_label != 'default' else ''}"
+    commit_url = f"https://{api_prefix}/{config['project']['name']}/commit/{commit_id if commit_id else ''}{f'?batch={quote(batch_label)}' if batch_label != 'default' else ''}"
     if is_ci or ctx.obj['share']:
       if status == "starting":
         click.echo(click.style("Results: ", bold=True) + click.style(commit_url, underline=True, bold=True), err=True)
