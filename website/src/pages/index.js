71600c4a8849c78d0e315423c285db149b0a7da2
Arthur Flam <arthur.flam@samsung.com>
2020-02-12 15:18:33 +0200

    Add github stars

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -59,6 +59,15 @@ function Home() {
             >
               Get Started
             </Link>
+            <span className={styles.indexCtasGitHubButtonWrapper}>
+              <iframe
+                className={styles.indexCtasGitHubButton}
+                src="https://ghbtns.com/github-btn.html?user=Samsung&amp;repo=qaboard&amp;type=star&amp;count=true&amp;size=large"
+                width={160}
+                height={30}
+                title="GitHub Stars"
+              />
+            </span>
           </div>
         </div>
       </header>
