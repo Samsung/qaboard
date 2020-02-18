b54e3e6d8ef13afbd34c884d124d5ab19ca475f8
itamarp <itamar.persi@partner.samsung.com>
2020-02-18 13:56:35 +0200

    improve visibility of "summary" errors

--- qaboard/qa.py
+++ qaboard/qa.py
@@ -669,7 +669,7 @@ def check_bit_accuracy_manifest(ctx, batches, batches_files):
       all_bit_accurate = all_bit_accurate and input_is_bit_accurate
 
     if not all_bit_accurate:
-      click.secho("\nError: you are not bit-accurate versus the manifest.", fg='red', underline=True, bold=True)
+      click.secho("\nError: you are not bit-accurate versus the manifest.", bg='red', underline=True, bold=True)
       click.secho("Reminder: the manifest lists the expected inputs/outputs for each test. It acts as an explicit gatekeeper against changes", fg='red', dim=True)
       if not input_database.is_absolute():
         click.secho("If that's what you wanted, update and commit all manifests.", fg='red')
@@ -750,7 +750,7 @@ def check_bit_accuracy(ctx, reference, batches, batches_files, reference_platfor
         for reference_commit in reference_commits:
           all_bit_accurate = is_bit_accurate(commit_dir, reference_rootproject_ci_dir, [o], reference_platform) and all_bit_accurate
     if not all_bit_accurate:
-      click.secho(f"ERROR: results are not bit-accurate to {reference_shas}.", fg='red', bold=True)
+      click.secho(f"\nERROR: results are not bit-accurate to {reference_shas}.", bg='red', bold=True)
       if is_ci:
         click.secho(f"\nTo investigate, go to", fg='red', underline=True)
         for reference_commit in reference_commits:
