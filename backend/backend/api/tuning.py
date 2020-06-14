7baa9376c47f72af6a8975f419b92e74b0864dc4
Arthur Flam <arthur.flam@samsung.com>
2020-06-14 23:08:40 +0300

    [SIRC] fix tuning syntax

--- backend/backend/api/tuning.py
+++ backend/backend/api/tuning.py
@@ -245,11 +245,6 @@ def start_tuning(hexsha):
         config_option = f"--tuning-search '{json.dumps(data['tuning_search'])}'"
 
     overwrite = "--action-on-existing run" if data["overwrite"] in ("on", True) else "--action-on-existing sync"
-            config_option,
-            f"{overwrite} --no-wait" if not do_optimize else '',
-            "\n",
-        ]
-    )
     batch_command = " ".join([
         "qa",
         f"--platform '{data['platform']}'" if "platform" in data else "",
