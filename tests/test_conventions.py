e2541cd2f2133c1b7e507d73277e70b6d0e44024
Arthur Flam <arthur.flam@samsung.com>
2020-08-04 10:29:43 +0300

    Tests for location specs

--- tests/test_conventions.py
+++ tests/test_conventions.py
@@ -4,8 +4,24 @@ https://docs.python.org/3/library/unittest.html
 """
 import unittest
 
+import os
+from pathlib import Path
 
 class TestConventions(unittest.TestCase):
+  def test_location_spec(self):
+    from qaboard.conventions import location_from_spec
+    self.assertEqual(location_from_spec({"linux": "/mnt/qaboard"}), Path("/mnt/qaboard"))
+    self.assertEqual(location_from_spec("/mnt/qaboard"), Path("/mnt/qaboard"))
+    os.environ['XXXXX'] = "user"
+    self.assertEqual(location_from_spec("/mnt/${XXXXX}"), Path("/mnt/user"))
+    self.assertEqual(location_from_spec("/mnt/{subproject.parts[0]}", {"subproject": Path("x/y")}), Path("/mnt/x"))
+    self.assertEqual(location_from_spec("/mnt/{subproject_parts[0]}", {"subproject_parts": ["x","y"]}), Path("/mnt/x"))
+    self.assertEqual(location_from_spec("/mnt/{subproject.name}", {"subproject": Path("x/y")}), Path("/mnt/y"))
+    # https://docs.python.org/3/library/string.html#formatspec
+    # https://www.python.org/dev/peps/pep-3101
+    # self.assertEqual(location_from_spec("/mnt/{subproject_parts[-1]}", {"subproject_parts": ["x","y"]}), Path("/mnt/y"))
+
+
   def test_serialize_config(self):
     from qaboard.conventions import serialize_config
     self.assertEqual(serialize_config(['a', 'b', 'c']), 'a:b:c')
