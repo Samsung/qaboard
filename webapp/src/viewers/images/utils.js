db577418d6e776268c511e7a734c2c8a2f7a0f08
Arthur Flam <arthur.flam@samsung.com>
2020-08-06 11:28:03 +0300

    [SIRC] Support new mounts

--- webapp/src/viewers/images/utils.js
+++ webapp/src/viewers/images/utils.js
@@ -1,8 +1,6 @@
 export const iiif_url = (output_dir_url, path) => {
-  // we only serve data from there
-  let identifier = output_dir_url.replace("/stage/algo_data", "")
   // remove the URL's leading "/s"
-  identifier = identifier.replace(/\/*?s\//, "")
+  let identifier = output_dir_url.replace(/\/*?s\//, "")
   // IIIF specs require encoding the slashes inside the identifier
   identifier = `${identifier}/${encodeURI(decodeURIComponent(path))}`.replace(/\//g, '%2F');
   let is_cde_file = identifier.endsWith('dng') || identifier.endsWith('raw') || identifier.endsWith('hex')
