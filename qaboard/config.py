0d7a72b7ffc524e9399218ca01a1a3f4a1a11ca3
Arthur Flam <arthur.flam@samsung.com>
2021-01-07 10:50:18 +0200

    [SIRC] helpful message if need to create new storage locations

--- qaboard/config.py
+++ qaboard/config.py
@@ -164,7 +164,10 @@ def mkdir(path: Path):
       config_has_error = True
       if not ignore_config_errors:
         click.secho(f'ERROR: The storage path does not exist: "{path}".', fg='red', err=True)
-
+        if "/algo/" in str(path) and not Path(*path.parts[:3]).exists():
+          click.secho(f'INFO: To create storage locations for new projects, go to:', fg='blue', bold=True, err=True)
+          click.secho(f'      http://projdb/ProjectSelect.php?NewProject=1', fg='blue', err=True)
+          click.secho(f'Products (HP1, 2X5...) are "Technology" projects, while CIS/KITT_ISP.. are "Group" projects.', fg='blue', err=True)
 
 outputs_root: Optional[Path]
 artifacts_root: Optional[Path]
