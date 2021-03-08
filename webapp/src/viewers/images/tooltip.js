d6f28d04a5ce218f4f5e87002e911bd3440800cd
Arthur Flam <arthur.flam@samsung.com>
2021-03-08 21:49:41 +0200

    Improve color tooltip

--- webapp/src/viewers/images/tooltip.js
+++ webapp/src/viewers/images/tooltip.js
@@ -9,11 +9,6 @@ import {
     Tooltip,
 } from "@blueprintjs/core";
 
-const margin = {marginLeft: '10px'};
-const colorFormat = color => color.toString().padStart('x', 3)
-const coordFormat = coord => Math.round(coord).toString().padStart('x', 5)
-
-
 // TODO
 // - unmount: cancel the request
 //      if (!!this.state.pixel_cancel_source && !!this.state.pixel_cancel_source.token)
@@ -21,15 +16,23 @@ const coordFormat = coord => Math.round(coord).toString().padStart('x', 5)
 // - if not fast enough: server keep image data (tile?)... (if multiprocess? could fill the server memory)
 // - when fractional values, merge... to get closer to the display rgb at low zoom
 
+const formatting = {
+    "dec": {base: 10, prefix: null},
+    "hex": {base: 16, prefix: '0x'},
+    "bin": {base:  2, prefix: '0b'},
+}
 
+const margin = {marginLeft: '10px'};
+const colorFormat = color => color.toString().padStart('x', 3)
 
 const ColorTooltip = ({color, x, y, image_url}) => {
+    const [base, setBase] = useState('dec');
     // we try to show the pixel data from the real image
     const [cancel_source, setCancelSource] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(null);
     const [pixel, setPixel] = useState(null)
-    
+
     const _debounce = useCallback(
         debounce( (_x, _y, _image_url) => {
             // console.log(`current ${x} ${y}`);
@@ -47,7 +50,11 @@ const ColorTooltip = ({color, x, y, image_url}) => {
                         image_url: _image_url,
                       }
                     const result = await axios.get("/api/v1/output/image/pixel", {params, cancelToken: new_cancel_source.token})
-                    setPixel(result.data)
+                    const value = Array.isArray(result.data.value) ? result.data.value : [result.data.value]
+                    setPixel({
+                        value,
+                        meta: result.data.meta,
+                    })
                     setLoading(false)
                     setError(null)
                 } catch(e) {
@@ -70,29 +77,39 @@ const ColorTooltip = ({color, x, y, image_url}) => {
     const color_diplayed = `RGB-display(${colorFormat(r)}, ${colorFormat(g)}, ${colorFormat(b)})`;
     
     const { value=[], meta={} } = pixel || {}
-    const type = meta.mode ?? meta.type ?? 'data'
-    const color_raw = `${type}(${value.join(', ')})`;
-    const color_hex = `${type}(${value.map(v => `0x${v.toString(16)}`).join(', ')})`;
-
+    const type = meta.mode ?? meta.imageType ?? 'data'
+    // const color_raw = `${type}(${value.join(', ')})`;
+    // const color_hex = `${type}(${value.map(v => `0x${v.toString(16).padStart('x', 3)}`).join(', ')})`;
     const hide_diplay = value.length === 3 && value[0] === r && value[1] === g && value[2] === b
 
+    const prefix = <span className={Classes.TEXT_MUTED}>{formatting[base].prefix}</span>
     return <span style={margin}>
         <Tag style={{background: color, ...margin}} round></Tag>
         {!!pixel && <>
             <Tooltip>
-                <code className={loading ? Classes.TEXT_MUTED: undefined} style={margin}>{color_raw}</code>
-                <p><code style={margin}>{color_hex}</code></p>
+                <code
+                    onClick={() => setBase(base === 'dec' ? 'hex' : 'dec')}
+                    className={loading ? Classes.TEXT_MUTED: undefined}
+                    style={margin}
+                >
+                    {type}({value.map((v, idx) => <span key={idx}>{prefix}{v.toString(formatting[base].base)}</span>).reduce((acc, x) => acc === null ? [x] : [acc, ', ', x], null)})
+                </code>
+                <p>Click to toggle hex/decimal numbers</p>
             </Tooltip>
             {error && <Tooltip>
                 <Icon icon="warning-sign" intent="danger"/>
                 <p>{JSON.stringify(error)}</p>
             </Tooltip>}
         </>}
-        {!hide_diplay && <code style={margin}>{color_diplayed}</code>}
+        {!hide_diplay && <Tooltip>
+            <code style={margin}>{color_diplayed}</code>
+            <li>Those values are what's displayed on your screen at this pixel</li>
+        </Tooltip>}
     </span>
 }
 
 
+const coordFormat = coord => Math.round(coord).toString().padStart('x', 5)
 
 const CoordTooltip = ({x, y}) => {
     if (x === undefined || x === null || y === undefined || y === null)
