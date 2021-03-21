94ff4f3d45195e3a7376d356e20072d2ebb2b9ad
Arthur Flam <arthur.flam@samsung.com>
2021-03-21 17:56:14 +0200

    Support --shallow and bind to home dirs

--- at-sirc-before-up.py
+++ at-sirc-before-up.py
@@ -4,6 +4,7 @@ Run this before calling `docker-compose up` at SIRC.
 
 At SIRC we rely on auto-mounted volumes. Attempting to mount them before they are bound leads to "too many levels of symbolic links errors".
 """
+import sys
 from pathlib import Path
 import yaml
 
@@ -12,6 +13,7 @@ sirc_config_path = Path(__file__).parent / 'sirc.yml'
 with sirc_config_path.open() as f:
     sirc_config = yaml.load(f)
 volumes = sirc_config['services']['proxy']['volumes']
+volumes.append("/home:/home")
 volumes = [Path(v.split(':')[0]) for v in volumes]
 
 for v in volumes:
@@ -25,6 +27,8 @@ for v in volumes:
         if d.is_file():
             continue
         try:
+            if '--shallow' in sys.argv:
+                continue
             for dd in d.iterdir():
                 print(f". . {dd}")
                 # print(dd)
