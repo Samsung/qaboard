eb8a2851e6284c3ecd2b7078d88ef433716c63f4
Arthur Flam <arthur.flam@samsung.com>
2021-04-05 15:57:35 +0300

    Better UI

--- webapp/src/components/authentication/Auth.js
+++ webapp/src/components/authentication/Auth.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { post, get } from "axios";
+import { connect } from 'react-redux'
 import {
   Classes,
   Intent,
@@ -11,53 +12,35 @@ import {
   Dialog,
   Toaster,
 } from "@blueprintjs/core";
-import { logIn, logOut } from '../../actions/users'
+import { login, logout } from '../../actions/users'
+
+// TODO:
+// - connect  auth to the store, get user from there
+// - check the flow
+// - start-server.md add LDAP
+// - sign-up ?
 
 const toaster = Toaster.create();
 
-class Auth extends React.Component {
+class AuthButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      is_loading: true,
-      is_authenticated: false,
-      user_id: '',
-      user_name: '',
-      full_name: '',
-      email : '',
-      is_ldap : false,
+      is_loading: false,
     };
-
-    this.getAuth = this.getAuth.bind(this);
   }
 
-  getAuth() {
+  checkAuth = () => {
     get("/api/v1/user/me/")
     .then(response => {
-      const {
-        is_authenticated,
-        user_id,
-        user_name,
-        full_name,
-        email ,
-        is_ldap,
-      } = response.data;
-      
+      const { is_authenticated, user_id, user_name, full_name, email, is_ldap } = response.data;
       this.setState({
-        is_authenticated,
-        user_id,
-        user_name,
-        full_name,
-        email ,
-        is_ldap,
         is_loading: false,
       });
-
       if (is_authenticated) {
-        this.props.dispatch(logIn(user_name))
-      }
-      else {
-        this.props.dispatch(logOut())
+        this.props.dispatch(login({user_name, email, is_ldap, full_name, user_id}))
+      } else {
+        this.props.dispatch(logout())
       }
     })
     .catch(error => {
@@ -67,33 +50,22 @@ class Auth extends React.Component {
   }
 
   componentDidMount() {
-    // check against session cookie?
-    this.getAuth()
+    if (!this.props.user.is_logged)
+      this.checkAuth()
   }
-
-  // check against session cookie?
-  // check on componentDidUpdate?
-  /*
-  componentDidUpdate() {
-    this.getAuth()
-  }
-  */
 
   render() {
-    const {
-      is_loading,
-      is_authenticated,
-    } = this.state;
+    if (this.state.loading)
+      return <Button loading={true}/>
 
-    const app_sider = this.props.appSider;
-    const loading = <Button loading={true}/>
-    const display = is_authenticated ?
-                    <UserMenu getAuth={this.getAuth} {...this.state}/> : 
-                    <Login getAuth={this.getAuth} appSider={app_sider} {...this.state}/>
-
-    return <>
-      {is_loading? loading : display}
-    </>
+    console.log("user", this.props.user)
+    return this.props.user?.is_authenticated ?
+              <UserMenu user={this.props.user}/> 
+            : <LoginButton
+                user={this.props.user}
+                dispatch={this.props.dispatch}
+                appSider={this.props.appSider}
+              />
   }
 }
 
@@ -121,13 +93,8 @@ class UserMenu extends React.Component {
   }
 
   render() {
-    const {
-      user_name,
-      full_name,
-    } = this.props;
-
-    const display_name = full_name || user_name
-
+    const { user_name, full_name } = this.props.user;
+    const display_name = full_name ?? user_name
     return <>
         <MenuItem
           text={<Icon icon="user"iconSize={Icon.SIZE_LARGE}/>}
@@ -152,7 +119,7 @@ class UserMenu extends React.Component {
 }
 
 
-class Login extends React.Component {
+class LoginButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -164,21 +131,23 @@ class Login extends React.Component {
       error : null,
       is_loading: false,
     };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.target);
     this.setState({is_loading: true});
 
     post("/api/v1/user/auth/", data)
     .then(response => {
-      this.props.getAuth()
-      this.handleClose()
-      const display_name = response.data.full_name ?? response.data.user_name
-      toaster.show({ message: `Welcome, ${display_name}`, intent: Intent.SUCCESS, timeout: 3000 });
+      const { user_id, user_name, full_name, email, is_ldap } = response.data;
+      toaster.show({ message: `Welcome, ${full_name ?? user_name}`, intent: Intent.SUCCESS, timeout: 3000 });
+      this.props.dispatch(login({user_name, email, is_ldap, full_name, user_id}))
+      this.setState({
+        error: null,
+        is_loading: false,
+        isOpen: false,
+      });
     })
     .catch(error => {
       let error_msg = error.response?.data?.error ?? "unknown-error"
@@ -186,9 +155,10 @@ class Login extends React.Component {
         error: error_msg,
         is_loading: false,
       });
-      toaster.show({ message: `ERROR: ${error_msg}`, intent: Intent.DANGER, timeout: 5000 })
+      if (!error_msg.startsWith('invalid'))
+        toaster.show({ message: `ERROR: ${error_msg}`, intent: Intent.DANGER, timeout: 5000 })
       console.log(error.response)
-    })
+      })
   }
 
 
@@ -205,35 +175,33 @@ class Login extends React.Component {
     };
 
     const key_state = {
-      text: "Log in",
       onClick: this.handleOpen,
     };
 
-    const key = this.props.appSider ?
-      <MenuItem icon="log-in" {...key_state}/> :
-      <Button className={Classes.MINIMAL} icon={<Icon icon="log-in" color="#106ba3"/>} style={{color : "#0d4975"}} {...key_state}/>
-
+    const login_button = this.props.appSider ?
+      <MenuItem icon="log-in" text="Login" {...key_state}/> :
+      <Button intent={Intent.PRIMARY} icon={<Icon icon="log-in" color="#fff"/>} style={{color : "#fff"}} text="Login" {...key_state}/>
+    const logout_button = this.props.appSider ?
+      <MenuItem icon="log-out" {...key_state} text="Logout"/> :
+      <Button icon={<Icon icon="log-out" color="#fff"/>} style={{color : "#fff"}} {...key_state} text="Logout"/>
     return <>
-      {key}
+      {!this.props.user.is_logged ? login_button : logout_button}
       <Dialog
           icon="log-in"
-          title="Log Into QA-Board"
+          title="Login"
           onClose={this.handleClose}
           style={{ width: "396px" }}
           {...this.state}
       >
         <form onSubmit={this.handleSubmit}>
           <div className={Classes.DIALOG_BODY}>
-            {/* <div style={{padding: "16px 0 16px 0"}}>
-              <p style={{textAlign: "center", fontSize: "large"}}>Log Into QA-Board</p>
-            </div> */}
             <div style={{padding: "6px"}}>
-              <InputGroup id="username" name="username" type="text" placeholder="User Name" autoFocus large  {...(error === "invalid-username" && warning)}/>
-              {error === "invalid-username" && <div style={{color: "#f02849", margin: "8px"}}>The user name you’ve entered doesn’t match any account.</div>}
+              <InputGroup id="username" name="username" type="text" placeholder="username" autoFocus large  {...(error === "invalid-username" && warning)}/>
+              {error === "invalid-username" && <div style={{color: "#f02849", margin: "8px"}}>This username does not match any user account.</div>}
             </div>
             <div style={{padding: "6px"}}>
-              <InputGroup id="password" name="password" type="password" placeholder="Password" large {...(error === "invalid-password" && warning)}/>
-              {error === "invalid-password" && <div style={{color: "#f02849", margin: "8px"}}>The password you’ve entered is incorrect.</div>}
+              <InputGroup id="password" name="password" type="password" placeholder="********" large {...(error === "invalid-password" && warning)}/>
+              {error === "invalid-password" && <div style={{color: "#f02849", margin: "8px"}}>The password is incorrect.</div>}
             </div>
             <div style={{padding: "6px"}} >
               <Button type="submit" large intent={Intent.PRIMARY} fill loading={this.state.is_loading}>
@@ -255,4 +223,9 @@ class Login extends React.Component {
 }
 
 
-export { Auth };
+const mapStateToProps = state => {
+  return {
+    user: state.user || null,
+  }
+}
+export default connect(mapStateToProps)(AuthButton);
