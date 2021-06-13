8ab45a0ccf6a4c774c55187fdb37c216e165e6d2
Arthur Flam <arthur.flam@samsung.com>
2021-06-13 10:29:41 +0300

    [sirc] drive mapping

--- qaboard/compat.py
+++ qaboard/compat.py
@@ -84,6 +84,7 @@ mappings = (
   ('\\\\mars\\stage\\jenkins_ws\\', '/stage/jenkins_ws/'),
   ('\\\\mars\\stage\\algo_jenkins_ws\\', '/stage/algo_jenkins_ws/'),
   ('\\\\mars\\raid\\data\\DATASYNC', '/raid/data/DATASYNC'),
+  ('\\\\netapp\\algo_ws', '/algo/ws'),
   ('\\\\mars\\raid\\algo\\', '/algo/'),
   ('\\\\mars\\algo\\', '/algo/'),
   ('\\\\mars\\raid\\', '/raid'),
