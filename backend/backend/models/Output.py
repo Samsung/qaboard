8e7760b4e31589f8b6a562a9d994713b1c6d477d
Arthur Flam <arthur.flam@samsung.com>
2021-07-25 23:39:54 +0300

    Delete file not whole folder..

--- backend/backend/models/Output.py
+++ backend/backend/models/Output.py
@@ -307,7 +307,7 @@ class Output(Base):
             continue
           print(f'{output_file}')
           if not dryrun:
-            rmtree(output_dir)
+            rmtree(output_file)
     self.deleted = True
 
 
