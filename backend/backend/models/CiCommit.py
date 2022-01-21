e362469cc3f13a0c33bc08ca5f12ef8e2d726b48
Arthur Flam <arthur.flam@samsung.com>
2022-01-21 16:01:05 +0200

    [SIRC] guests have no avatar

--- backend/backend/models/CiCommit.py
+++ backend/backend/models/CiCommit.py
@@ -325,8 +325,11 @@ class CiCommit(Base):
         committer_avatar_url = f'http://gravatar.com/avatar/{name_hash}'
       else:
         committer_avatar_url = user['avatar_url']
-        if "gravatar" in committer_avatar_url:
-          committer_avatar_url = f"https://dag.sirc.co.il:8081/{user['username']}.jpg"
+        if "gravatar" in committer_avatar_url and 'username' in user:
+          # only SIRC users have avatars...
+          identities = user.get('identities', [])
+          if all(['ou=guests' not in i['extern_uid'] for i in identities]):
+            committer_avatar_url = f"https://dag.sirc.co.il:8081/{user['username']}.jpg"
     repo_artifacts_url = self.repo_artifacts_url
     artifacts_url = self.artifacts_url
     out = {
