f60c227143156aea3d2c44c71efc757a4009480f
Arthur Flam <arthur.flam@samsung.com>
2022-02-09 04:33:06 +0900

    Celerey runners share ENV variables with the calling command

--- qaboard/runners/celery_app.py
+++ qaboard/runners/celery_app.py
@@ -4,11 +4,11 @@ import subprocess
 from celery import Celery
 
 app = Celery('celery_app')
-app.conf.update(
-    broker_url=os.environ.get('CELERY_BROKER_URL', 'pyamqp://guest@localhost//'),
+app.conf.update(  
+    broker_url=os.environ.get('CELERY_BROKER_URL', 'pyamqp://guest:guest@qaboard:5672//'),
     result_backend=os.environ.get('CELERY_RESULT_BACKEND', 'rpc://'),
     task_serializer='pickle',
-    accept_content=['pickle'],
+    accept_content=['pickle', 'json'],
     result_serializer='pickle',
     enable_utc=True,
 )
@@ -18,18 +18,22 @@ celery_config = config.get('runners', {}).get('celery', {})
 app.conf.update(**celery_config)
 
 
+
 @app.task(bind=True, name=celery_config.get('qaboard_task_name', "qaboard"))
-def start(self, job, cwd=None):
+def start(self, job, cwd=None, env=None):
   # https://docs.celeryproject.org/en/stable/userguide/tasks.html#task-request-info
   print('Executing task id {0.id}, groupID: {0.group}'.format(self.request))
 
   pipe = subprocess.PIPE
+  # print("job.run_context.command", job.run_context.command)
+  # print("env", env)
   with subprocess.Popen(job.run_context.command, shell=True,
                         encoding='utf-8',
                         # Avoid issues with code outputing malformed unicode
                         # https://docs.python.org/3/library/codecs.html#error-handlers
                         errors='surrogateescape',
                         cwd=cwd if cwd else job.run_context.job_options['cwd'],
+                        env=env,
                         stdout=pipe, stderr=pipe) as process:
     for line in process.stdout:
       print(line, end='')
