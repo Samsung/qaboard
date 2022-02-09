8cd319c7b4888edeb6d98bb41afc017f43a7013f
Arthur Flam <arthur.flam@samsung.com>
2022-02-09 11:31:03 +0200

    Fix: Types

--- qaboard/utils.py
+++ qaboard/utils.py
@@ -126,7 +126,7 @@ def entrypoint_module(config):
       name = f'qaboard-entrypoint'
       # https://docs.python.org/3/library/importlib.html#importing-a-source-file-directly
       spec = importlib.util.spec_from_file_location(name, entrypoint)
-
+      assert spec
       module = importlib.util.module_from_spec(spec)
       sys.path.insert(0, str(entrypoint.parent))
       spec.loader.exec_module(module)
