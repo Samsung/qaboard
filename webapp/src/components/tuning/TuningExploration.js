8a56251dbd87593fff3c3d5c40aab0307d7c138b
Liran Hareli <liran.hareli@samsung.com>
2022-03-08 16:29:13 +0000

    In optimization mode some of the data include NaNs so there is a need to connect the gaps in the 2d plot
    https://plotly.com/python/contour-plots/#:~:text=16-,Connect%20the%20Gaps%20Between%20None%20Values%20in%20the%20Z%20Matrix,-import%20plotly.graph_objs

--- webapp/src/components/tuning/TuningExploration.js
+++ webapp/src/components/tuning/TuningExploration.js
@@ -454,6 +454,7 @@ const Sensibility2DContour = React.memo(({
   layout,
   available_metrics,
   aggregation,
+  connectgaps_enable
 }) => {
   // https://plot.ly/javascript/reference/#contour
   // https://plot.ly/javascript/contour-plots/
@@ -527,7 +528,7 @@ const Sensibility2DContour = React.memo(({
         }
       },
       // zsmooth: 'best',// default
-      connectgaps: false,
+      connectgaps: connectgaps_enable,
       colorscale: "Viridis"
       // reversescale: true,
       // showscale: false,
@@ -771,6 +772,7 @@ class TuningExploration extends Component {
             available_metrics={available_metrics}
             parameters={[selected_parameter, selected_parameter_2]}
             aggregation={this.state.aggregation}
+            connectgaps_enable={batch_data.optimization} // In optimization mode some of the data include NaNs so there is a need to connect the gaps in the 2d plot	
           />
           <div className={Classes.TEXT_MUTED} style={{ fontSize: 10 }}>
             <p><span style={{borderBottom: '1px dashed #999', textDecoration: 'none'}} title={`${aggregation} over all selected inputs`}>Aggregated scores</span> are computed for each set of tuning parameters.</p>
