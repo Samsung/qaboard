6a4f2ab15802ee4f691a1319b2208a3b820d4353
Arthur Flam <arthur.flam@samsung.com>
2022-03-16 09:39:21 +0200

    Avoid duplicated prefix in docs used in  webapp

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to={useBaseUrl('docs/introduction')}
+            to={useBaseUrl('docs/introduction').replace('docs/docs/', 'docs/')}
           >
             Get Started
           </Link>
