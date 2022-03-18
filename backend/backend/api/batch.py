9bbbbb6825c097ce25c054998eee32ecde26a843
Chen Rimoch <chen.rimoch@samsung.com>
2022-03-18 16:17:43 +0000

    Update batch.py

--- backend/backend/api/batch.py
+++ backend/backend/api/batch.py
@@ -55,11 +55,12 @@ def update_batch():
       flag_modified(batch, "data")
       # we will save the outputs from the best iteration in the batch,
       # so first we need to remove any previous best results
-      for o in batch.outputs:
-        if o.output_type != 'optim_iteration':
-          print(f"  DELETE {o}")
-          o.delete(soft=False)
-          db_session.delete(o)
+      if batch_data.get("keep_all_best_iters") == 0: # Delete all "best_iters" (by default)
+        for o in batch.outputs:
+          if o.output_type != 'optim_iteration':
+            print(f"  DELETE {o}")
+            o.delete(soft=False)
+            db_session.delete(o)
       db_session.add(batch)
       db_session.commit()
 
