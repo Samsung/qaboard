c59951aa107bdf49f76ef4a149e9f14834cfdbc5
Arthur Flam <arthur.flam@samsung.com>
2022-03-28 21:00:51 +0300

    Avoid sending too many metrics via URLs for nothing

--- qaboard/api.py
+++ qaboard/api.py
@@ -235,7 +235,12 @@ def get_outputs(qa_context: Optional[Dict[str, Any]]) -> Dict[int, Any]:
   if not should_notify_qa_database:
     return {}
   try:
-    return batch_info(reference=commit_id, batch=qa_context['batch_label'])['outputs']
+    return batch_info(
+      reference=commit_id,
+      batch=qa_context['batch_label'],
+      # we don't need any metric when calling this function from "qa batch", just the output dirs / configs 
+      metrics={"none-required": None},
+    )['outputs']
   except:
     return {}
 
