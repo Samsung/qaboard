6b1bb3e1c6a0405b3be8553c39bd91986f775843
Arthur Flam <arthur.flam@samsung.com>
2022-04-18 15:01:50 +0300

    Use the same tags in logs as in other visualizations

--- webapp/src/components/BatchLogs.js
+++ webapp/src/components/BatchLogs.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { InView } from 'react-intersection-observer'
 import { get } from "axios";
 
 import { DateTime } from 'luxon';
@@ -37,7 +38,12 @@ class OutputLog extends React.Component {
       error: null,
       logs: null,
       logs_html: null,
+      viewable: false || props.viewable,
     };
+  }
+
+  becameViewable = inView => {
+    this.setState({viewable: true})
   }
 
   componentDidUpdate(prevProps) {
@@ -136,8 +142,8 @@ class OutputLog extends React.Component {
   }
 
   render() {
-    const { output } = this.props;
-    const { is_open, is_loaded, error, logs_html_safe } = this.state;
+    const { output, commit, project, dispatch } = this.props;
+    const { is_open, is_loaded, error, logs_html_safe, viewable } = this.state;
     // console.log(`[logs] render ${output.test_input_path}`)
     // console.log(sanitizeHtml("<Config>test</Config>"));
 
@@ -154,14 +160,18 @@ class OutputLog extends React.Component {
     </>
     return (
       <div>
+        {!viewable && <InView key="unviewable" threshold={0.1} margin='150%' /*triggerOnce*/ onChange={inView => this.becameViewable(inView)}>
+          <span key="viewable"></span>
+        </InView>}
         <OutputHeader
-          project={this.props.project}
-          commit={this.props.commit}
+          project={project}
+          commit={commit}
           output={output}
           mismatch={output.reference_mismatch}
-          dispatch={this.props.dispatch}
+          dispatch={dispatch}
           prefix={header_prefix}
           tags_first
+          viewable={viewable}
         />
         <Collapse isOpen={is_open}>
           {error ? 
