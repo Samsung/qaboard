755cee81c1bdbabc4cbf3f5cb7b98c6fe5d75145
Arthur Flam <arthur.flam@samsung.com>
2023-08-24 08:29:34 +0300

    Use non-deprecated name

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
     'simplejson',
     'pyyaml',      # YAML reader
     'joblib',      # Parallelism for dummies
-    'sklearn',
+    'scikit-learn',
     'scikit-optimize',
     # Only at SIRC
     'cde @ git+ssh://git@gitlab-srv/cde/cde-python',
