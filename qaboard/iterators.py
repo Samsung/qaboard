053dd79892d8946ac2281cc26a6b03fec6a9e584
nadav oxenberg <nadav.o@samsung.com>
2024-03-17 10:25:58 +0200

    add docs

--- qaboard/iterators.py
+++ qaboard/iterators.py
@@ -111,7 +111,7 @@ def iter_inputs_at_path(path, database, globs, use_parent_folder, qatools_config
         metadata_keys = list(metadata_keys.keys())
         for k in metadata_keys:
           if k.startswith('_') or k == "input_path":
-            del metadata[key]
+            del metadata[k]
         nb_inputs += 1
         yield i, database
 
