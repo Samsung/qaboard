e22271c81194aedc6d5da99957f584774e7cc334
Idan Matalon <idan.matalon@partner.samsung.com>
2024-06-13 19:21:45 +0300

    retry requests.post 3 times if has Exception

--- qaboard/gitlab.py
+++ qaboard/gitlab.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import time
 
 import requests
 import click
@@ -53,20 +54,27 @@ def update_gitlab_status(state, name, target_url, description, commit_id=commit_
     "description": description,
     # "ref": commit_branch,
   }
-  try:
-    r = requests.post(url, headers=gitlab_headers, params=params)
-  except Exception as e:
-    print(e)
-    print(url)
+  
+  max_retries = 3
+  retry_delay = 3  # seconds
+
+  for attempt in range(max_retries):
+      try:
+          r = requests.post(url, headers=gitlab_headers, params=params)
+          break
+      except Exception as e:
+          print(f"Attempt {attempt + 1} failed: {e}")
+          if attempt < max_retries - 1:  
+              time.sleep(retry_delay)  
+          else:
+              print(e)
+              print(url)
   try:
     r.raise_for_status()
   except Exception as e:
     print(e)
     print(url)
     print(r)
-
-
-
 
 
 def lastest_successful_ci_commit(commit_id: str, max_parents_depth=config.get('bit_accuracy', {}).get('max_parents_depth', 5)):
