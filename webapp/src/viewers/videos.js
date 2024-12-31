6f5159acc75e396b1bb5f26571becf5ae1c8439d
Arthur Flam <arthur.flam@samsung.com>
2024-12-31 11:59:00 +0200

    Add current time display and improve sync

--- webapp/src/viewers/videos.js
+++ webapp/src/viewers/videos.js
@@ -1,129 +1,212 @@
-import React from "react";
-
-import {
-  Tag,
-} from "@blueprintjs/core";
+import { useState, useEffect, useRef } from 'react';
+import { Tag } from '@blueprintjs/core';
 
 import { is_same_data } from "../utils"
 
-class SyncedVideos extends React.Component {
-  constructor(props) {
-    super(props);
-    this.viewNewR = React.createRef();
-    this.viewRefR = React.createRef();
+const SyncedVideos = ({
+  output_new,
+  output_ref,
+  path,
+  poster = 'poster.jpg',
+  type,
+  style,
+  manifests,
+}) => {
+  // the viewer for the new video is always shown
+  const viewer_new_ref = useRef(null);
+
+  // but it's not the case for the reference
+  const viewer_reference_ref = useRef(null);
+  const [show_reference, setShowReference] = useState(false);
+
+  const [currentTimeNew, setCurrentTimeNew] = useState(0);
+  const [currentTimeRef, setCurrentTimeRef] = useState(0);
+
+
+  // we cannot "play" videos that are not ready
+  const [is_ready_video_new, setNewVideoReady] = useState(false);
+  const [is_ready_video_reference, setRefVideoReady] = useState(false);
+
+  const meta_new = manifests?.new?.[path]
+  const meta_reference = manifests?.reference?.[path]
+  const hasSameData = is_same_data(path, meta_new, meta_reference);
+
+  const show_reference_updated = !!output_ref && !hasSameData
+  if (show_reference !== show_reference_updated) {
+    setShowReference(show_reference_updated)
   }
-  play_ref = () => {
-    if(!!this.props.output_ref && !!this.viewRefR.current)
-      this.viewRefR.current.play()
+
+  useEffect(() => {
+    const handleTimeUpdate = () => {
+      setCurrentTimeRef(viewer_reference_ref.current.currentTime);
+    };
+
+    if (viewer_reference_ref.current) { // normally should be always true
+      viewer_reference_ref.current.addEventListener("timeupdate", handleTimeUpdate);
+      viewer_reference_ref.current.addEventListener('canplay', handleCanPlayRef);
+
+      viewer_reference_ref.current.addEventListener('play', handlePlayRef);
+      viewer_reference_ref.current.addEventListener('pause', handlePauseRef);
+      viewer_reference_ref.current.addEventListener('timeupdate', syncReferenceVideo);
+      viewer_reference_ref.current.addEventListener('seeked', syncReferenceVideoTwice);
+      viewer_reference_ref.current.addEventListener('seeking', syncReferenceVideoTwice);
+      viewer_reference_ref.current.addEventListener('waiting', syncReferenceVideoTwice);
+    }
+    // cleanup on unmount
+    return () => {
+      if (viewer_reference_ref.current) { // normally should be always true
+        viewer_reference_ref.current.removeEventListener("timeupdate", handleTimeUpdate);
+        viewer_reference_ref.current.removeEventListener('canplay', handleCanPlayRef);
+
+        viewer_reference_ref.current.removeEventListener('play', handlePlayRef);
+        viewer_reference_ref.current.removeEventListener('pause', handlePauseRef);
+        viewer_reference_ref.current.removeEventListener('timeupdate', syncReferenceVideo);
+        viewer_reference_ref.current.removeEventListener('seeked', syncReferenceVideoTwice);
+        viewer_reference_ref.current.removeEventListener('seeking', syncReferenceVideoTwice);
+        viewer_reference_ref.current.removeEventListener('waiting', syncReferenceVideoTwice);
+      }
+    };
+  }, [show_reference])
+
+ 
+  useEffect(() => {
+    const handleTimeUpdate = () => {
+      setCurrentTimeNew(viewer_new_ref.current.currentTime);
+    };
+    if (viewer_new_ref.current) { // always true since unconditionnaly rendered
+      viewer_new_ref.current.addEventListener("timeupdate", handleTimeUpdate);
+      viewer_new_ref.current.addEventListener('canplay', handleCanPlayNew);
+
+      viewer_new_ref.current.addEventListener('play', handlePlayRef);
+      viewer_new_ref.current.addEventListener('pause', handlePauseRef);
+      viewer_new_ref.current.addEventListener('timeupdate', syncReferenceVideo);
+      viewer_new_ref.current.addEventListener('seeked', syncReferenceVideoTwice);
+      viewer_new_ref.current.addEventListener('seeking', syncReferenceVideoTwice);
+      viewer_new_ref.current.addEventListener('waiting', syncReferenceVideoTwice);
+    }
+    // cleanup on unmount
+    return () => {
+      if (viewer_new_ref.current) {
+        viewer_new_ref.current.removeEventListener("timeupdate", handleTimeUpdate);
+        viewer_new_ref.current.removeEventListener('canplay', handleCanPlayNew);
+
+        viewer_new_ref.current.removeEventListener('play', handlePlayRef);
+        viewer_new_ref.current.removeEventListener('pause', handlePauseRef);
+        viewer_new_ref.current.removeEventListener('timeupdate', syncReferenceVideo);
+        viewer_new_ref.current.removeEventListener('seeked', syncReferenceVideoTwice);
+        viewer_new_ref.current.removeEventListener('seeking', syncReferenceVideoTwice);
+        viewer_new_ref.current.removeEventListener('waiting', syncReferenceVideoTwice);
+      }
+    };
+  }, []);
+
+
+  // TODO: do we have to split in 2 functions for each ?
+  const handleCanPlayRef = () => {
+    setRefVideoReady(true);
   };
-  pause_ref = () => {
-    if(!!this.props.output_ref && !!this.viewRefR.current) {
-      this.viewRefR.current.pause()
-      this.syncReferenceVideo();
-    }
-  }
-  canplay_ref = () => {
-    this.ref_video_ready = true;
+  const handleCanPlayNew = () => {
+    setNewVideoReady(true);
   };
 
-  componentDidMount() { 
-    this.viewNewR.current.addEventListener("play", this.play_ref);
-    this.viewNewR.current.addEventListener("pause", this.pause_ref);
-
-    this.viewNewR.current.addEventListener('timeupdate', this.syncReferenceVideo);
-    this.viewNewR.current.addEventListener("seeked", this.syncReferenceVideoTwice);
-    this.viewNewR.current.addEventListener('seeking', this.syncReferenceVideoTwice);
-    this.viewNewR.current.addEventListener("waiting", this.syncReferenceVideoTwice);
-
-    if(!!this.viewRefR.current)
-      this.viewRefR.current.addEventListener("canplay", this.canplay_ref);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const has_same_data = is_same_data(this.props.path, this.props.manifests?.new?.[this.props.path], this.props.manifests?.reference?.[this.props.path])
-    const was_same_data = is_same_data(prevProps.path, prevProps.manifests?.new?.[prevProps.path], prevProps.manifests?.reference?.[prevProps.path])
-    if(was_same_data && !has_same_data && !!this.viewRefR.current)
-      this.viewRefR.current.addEventListener("canplay", this.canplay_ref);
-  }
-
-  componentWillUnmount() {
-    // Make sure to remove the DOM listener when the component is unmounted.
-    if (!!this.viewRefR.current) {
-      this.viewRefR.current.removeEventListener("loadedmetadata", this.canplay_ref);
+  const handlePlayRef = () => {
+    if (viewer_new_ref.current) {
+      let play_promise_new = viewer_new_ref.current.play();
+      play_promise_new.then(_ => {
+        console.log("play started (new)")
+      })
+      .catch(error => {
+        console.log("Error when playing (new)", error)
+      })
     }
-    if (!!this.viewNewR.current) {
-      this.viewNewR.current.removeEventListener("play", this.play_ref);
-      this.viewNewR.current.removeEventListener("pause", this.pause_ref);
-      this.viewNewR.current.removeEventListener("timeupdate", this.syncReferenceVideo);
-      this.viewNewR.current.removeEventListener("seeked", this.syncReferenceVideoTwice);      
-      this.viewNewR.current.removeEventListener("seeking", this.syncReferenceVideoTwice);      
-      this.viewNewR.current.removeEventListener("waiting", this.syncReferenceVideoTwice);      
+    if (viewer_reference_ref.current) {
+      syncReferenceVideoTwice()
+      let play_promise_ref = viewer_reference_ref.current.play();
+      play_promise_ref.then(_ => {
+        console.log("play started (ref)")
+      })
+      .catch(error => {
+        console.log("Error when playing (ref)", error)
+      })
+    }  
+  };
+
+  const handlePauseRef = () => {
+    if (viewer_new_ref.current) {
+      viewer_new_ref.current.pause();
     }
-  }
-
-  syncReferenceVideo = () => {
-    if (this.ref_video_ready) {
-      // console.log('befor:', this.viewNewR.current.currentTime, this.viewRefR.current.currentTime)
-      this.viewRefR.current.currentTime = this.viewNewR.current.currentTime;
-      // console.log('after:', this.viewNewR.current.currentTime, this.viewRefR.current.currentTime)
+    if (viewer_reference_ref.current) {
+      viewer_reference_ref.current.pause();
+      syncReferenceVideo();
     }
-  }
+  };
 
-  syncReferenceVideoTwice = () => {
-    if (this.ref_video_ready) {
-      // console.log('befor:', this.viewNewR.current.currentTime, this.viewRefR.current.currentTime)
-      this.viewRefR.current.currentTime = this.viewNewR.current.currentTime;
-      // console.log('after:', this.viewNewR.current.currentTime, this.viewRefR.current.currentTime)
-      setTimeout(100, () => {this.viewRefR.current.currentTime = this.viewNewR.current.currentTime})
-      setTimeout(200, () => {this.viewRefR.current.currentTime = this.viewNewR.current.currentTime})
+  const syncReferenceVideo = () => {
+    if (is_ready_video_reference) {
+      viewer_reference_ref.current.currentTime = viewer_new_ref.current.currentTime;
     }
-  }
+  };
 
-  render() {
-    const { output_new, output_ref, path, poster='poster.jpg', type, manifests } = this.props;
-    const has_same_data = is_same_data(path, manifests?.new?.[path], manifests?.reference?.[path])
-    let width = parseFloat(((this.props.style || {}).width || '390px').replace(/[^\d]+/, ''))
-    const single_video_width = (width - 10) / 2
+  const syncReferenceVideoTwice = () => {
+      syncReferenceVideo()
+      setTimeout(() => {
+        syncReferenceVideo();
+      }, 100);
+      setTimeout(() => {
+        syncReferenceVideo();
+      }, 200);
+  };
 
-    return (
-      <>
+  const width = parseFloat(((style?.width ?? '390px').replace(/[^\d]+/, '')))
+  const singleVideoWidth = (width - 10) / 2;
+
+  return (
+    <>
+      <div>
         <div>
-          <div><Tag intent="primary">new</Tag></div>
-          <video
-            ref={this.viewNewR}
-            preload="none"
-            controls
-            loop="loop"
-            title="New"
-            width={single_video_width}
-            poster={`${output_new.output_dir_url}/${poster}`}
-            type={type}
-          >
-            <source src={`${output_new.output_dir_url}/${path}`} />
-          </video>
+          <Tag intent="primary">new <code>@{currentTimeNew}s</code></Tag>
         </div>
-        {!!output_ref ? (
+        <video
+          ref={viewer_new_ref}
+          preload="none"
+          controls
+          loop="loop"
+          title="New"
+          width={singleVideoWidth}
+          poster={`${output_new.output_dir_url}/${poster}`}
+          type={type}
+        >
+          <source src={`${output_new.output_dir_url}/${path}`} />
+        </video>
+      </div>
+      {output_ref ? (
+        <div>
           <div>
-            <div>
-              <Tag intent="warning">{!has_same_data ? "reference" : 'reference (same-video)'}</Tag>
-            </div>
-            {!has_same_data && <video
-              ref={this.viewRefR}
+            <Tag intent="warning">
+              {!hasSameData ? 'reference' : 'reference (same-video)'}
+              <code>@{currentTimeRef}s</code>
+            </Tag>
+          </div>
+          {!hasSameData && (
+            <video
+              ref={viewer_reference_ref}
               controls
               preload="none"
               loop="loop"
               title="Reference"
-              width={single_video_width}
+              width={singleVideoWidth}
               poster={`${output_ref.output_dir_url}/${poster}`}
               type={type}
             >
               <source src={`${output_ref.output_dir_url}/${path}`} />
-            </video>}
-          </div>
-        ) : <span ref={this.viewRefR}/>}
-      </>
-    );
-  }
-}
+            </video>
+          )}
+        </div>
+      ) : (
+        <span ref={viewer_reference_ref} />
+      )}
+    </>
+  );
+};
 
 export default SyncedVideos;
