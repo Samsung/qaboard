e5f4a047b317ba6015bd1b49dae482a345712730
Arthur Flam <arthur.flam@samsung.com>
2025-02-26 18:25:07 +0200

    Faster failure case

--- backend/backend/api/commit.py
+++ backend/backend/api/commit.py
@@ -96,6 +96,11 @@ def api_ci_commit(commit_id=None):
                     )
         assert ci_commit
       except Exception as e:
+        # if the user provided what looks like a commit ID, we want to fail fast
+        # otherwise the code below is super slow
+        if re.match(r'^[0-9a-fA-F]{40}$', commit_id)
+          return jsonify({'error': f'Sorry, we could not find any data on commit ID {commit_id} in project {project_id}.'}), 404
+
         try:
           # TODO: This is a valid use case for having read-rights to the repo,
           #       we can identify a commit by the tag/branch
