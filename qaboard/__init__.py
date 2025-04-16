35552c6579bc824f1c7a6308ee9fa86b1509ce23
Arthur Flam <arthur.flam@samsung.com>
2025-04-16 16:06:00 +0300

    Move sentry integration to avoid being used when it's just a qaboard import

--- qaboard/__init__.py
+++ qaboard/__init__.py
@@ -1,22 +1,6 @@
 # need to be update setup.py as well
 __version__ = '1.0.2'
-import os
-if os.environ.get("CI"):
-    import urllib3
-    urllib3.disable_warnings()
-    import sentry_sdk
-    class InsecureHttpTransport(sentry_sdk.transport.HttpTransport):
-        def _get_pool_options(self):
-            options = super()._get_pool_options()
-            options["cert_reqs"] = "CERT_NONE" # Ignore SSL Errors
-            return options
-    sentry_sdk.init(
-        dsn="https://09ed52c49322629052df6b6e6cf334c5@sentry.transchip.com/30",
-        transport=InsecureHttpTransport,
-        # Set traces_sample_rate to 1.0 to capture 100%
-        # of transactions for tracing.
-        traces_sample_rate=1.0,
-    )
+
 
 from .check_for_updates import check_for_updates
 check_for_updates()
--- qaboard/qa.py
+++ qaboard/qa.py
@@ -902,6 +902,22 @@ def init(ctx):
 
 
 def main():
+  if os.environ.get("CI"):
+    import urllib3
+    urllib3.disable_warnings()
+    import sentry_sdk
+    class InsecureHttpTransport(sentry_sdk.transport.HttpTransport):
+      def _get_pool_options(self):
+        options = super()._get_pool_options()
+        options["cert_reqs"] = "CERT_NONE" # Ignore SSL Errors
+        return options
+    sentry_sdk.init(
+      dsn="https://09ed52c49322629052df6b6e6cf334c5@sentry.transchip.com/30",
+      transport=InsecureHttpTransport,
+      # Set traces_sample_rate to 1.0 to capture 100%
+      # of transactions for tracing.
+      traces_sample_rate=1.0,
+    )
   from .compat import ensure_cli_backward_compatibility
   ensure_cli_backward_compatibility()
   qa(obj={}, auto_envvar_prefix='QA')
