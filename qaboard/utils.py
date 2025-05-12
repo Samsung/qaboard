26246bd405960c7582d87e383e57ac949c8cd527
Arthur Flam <arthur.flam@samsung.com>
2025-05-12 11:41:06 +0300

    Store pixel-value hashes

--- qaboard/utils.py
+++ qaboard/utils.py
@@ -297,15 +297,23 @@ def _file_info(path : Path, compute_hashes=True):
     if compute_hashes:
         info['md5'] = md5_hex(path)
         image_meta = None
+
+        # At SIRC we compute hash that only depend
+        # on pixel values, not metadata
         if path.suffix == '.hex':
           from cde.image.read import hex_attributes
           hex_attr = hex_attributes(path)
           hash_length = hex_attr.get('footer_start_pos')
           info['md5_data'] = md5_hex(path, hash_length)
           image_meta = hex_attr
+        if path.suffix.lower() in [".png", ".bmp", ".jpg", ".jpeg"]:
+          from idb_client.v2.utils import Md5HashCalculator
+          info['md5_data'] = Md5HashCalculator.from_image(path)
+
         if path.suffix == '.raw':
           from cde.image.read import read_imgprops
           image_meta = read_imgprops(path)
+
         if image_meta:
           for attr in checked_cde_attrs:
             if attr in image_meta:
